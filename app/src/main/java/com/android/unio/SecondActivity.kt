3ec090753c65bc618bcca10634d5a232614c821b
Oskar Codes <oskarbertrand@icloud.com>
2024-10-04 19:18:34 +0200

    chore(format): Format code using ktfmtFormat.

--- app/src/main/java/com/android/unio/SecondActivity.kt
+++ app/src/main/java/com/android/unio/SecondActivity.kt
@@ -7,11 +7,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTag
-import androidx.compose.ui.tooling.preview.Preview
 import com.android.unio.resources.C
 import com.android.unio.ui.theme.SampleAppTheme
 
@@ -24,9 +22,10 @@ class SecondActivity : ComponentActivity() {
         Surface(
             modifier = Modifier.fillMaxSize().semantics { testTag = C.Tag.second_screen_container },
             color = MaterialTheme.colorScheme.background) {
-            Text("Hello Robolectric!", modifier = Modifier.semantics { testTag = C.Tag.greeting_robo })
-
-        }
+              Text(
+                  "Hello Robolectric!",
+                  modifier = Modifier.semantics { testTag = C.Tag.greeting_robo })
+            }
       }
     }
   }
