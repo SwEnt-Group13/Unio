eb8d2410e4b7ebdd0c2e5e2a0afaaacf0e73b9e2
Oskar Codes <oskarbertrand@icloud.com>
2024-10-04 17:34:12 +0200

    chore(MainActivity): Add MainActivity entry point to com.android.unio.

---
+++ app/src/main/java/com/android/unio/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.android.unio
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.testTag
+import com.android.sample.resources.C
+import com.android.sample.ui.theme.SampleAppTheme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      SampleAppTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize().semantics { testTag = C.Tag.main_screen_container },
+            color = MaterialTheme.colorScheme.background) {
+              Text("Unio App")
+            }
+      }
+    }
+  }
+}
