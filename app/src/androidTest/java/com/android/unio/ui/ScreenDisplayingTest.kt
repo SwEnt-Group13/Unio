aca411afd13db2b472a83192f756e5b78869ad24
Romain Hirschi <romain.hirschi@epfl.ch>
2024-10-07 16:31:23 +0200

    test: add test to assert content of screens are displayed

--- app/src/androidTest/java/com/android/unio/ui/ScreenDisplayingTest.kt
+++ app/src/androidTest/java/com/android/unio/ui/ScreenDisplayingTest.kt
@@ -3,6 +3,7 @@ package com.android.unio.ui
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
 import com.android.unio.ui.association.AssociationScreen
 import com.android.unio.ui.authentication.LoginScreen
 import com.android.unio.ui.event.EventCreationScreen
@@ -25,65 +26,76 @@ class ScreenDisplayingTest() {
   fun testHomeDisplayed() {
     composeTestRule.setContent { HomeScreen() }
     composeTestRule.onNodeWithTag("HomeScreen").assertIsDisplayed()
+    composeTestRule.onNodeWithText("Home screen").assertIsDisplayed()
   }
 
   @Test
   fun testExploreDisplayed() {
     composeTestRule.setContent { ExploreScreen() }
     composeTestRule.onNodeWithTag("ExploreScreen").assertIsDisplayed()
+    composeTestRule.onNodeWithText("Explore screen").assertIsDisplayed()
   }
 
   @Test
   fun testMapDisplayed() {
     composeTestRule.setContent { MapScreen() }
     composeTestRule.onNodeWithTag("MapScreen").assertIsDisplayed()
+    composeTestRule.onNodeWithText("Map screen").assertIsDisplayed()
   }
 
   @Test
   fun testEventDisplayed() {
     composeTestRule.setContent { EventScreen() }
     composeTestRule.onNodeWithTag("EventScreen").assertIsDisplayed()
+    composeTestRule.onNodeWithText("Event screen").assertIsDisplayed()
   }
 
   @Test
   fun testEventCreationDisplayed() {
     composeTestRule.setContent { EventCreationScreen() }
     composeTestRule.onNodeWithTag("EventCreationScreen").assertIsDisplayed()
+    composeTestRule.onNodeWithText("Event creation screen").assertIsDisplayed()
   }
 
   @Test
   fun testLoginDisplayed() {
     composeTestRule.setContent { LoginScreen() }
     composeTestRule.onNodeWithTag("LoginScreen").assertIsDisplayed()
+    composeTestRule.onNodeWithText("Login screen").assertIsDisplayed()
   }
 
   @Test
   fun testAssociationDisplayed() {
     composeTestRule.setContent { AssociationScreen() }
     composeTestRule.onNodeWithTag("AssociationScreen").assertIsDisplayed()
+    composeTestRule.onNodeWithText("Association screen").assertIsDisplayed()
   }
 
   @Test
   fun testSavedDisplayed() {
     composeTestRule.setContent { SavedScreen() }
     composeTestRule.onNodeWithTag("SavedScreen").assertIsDisplayed()
+    composeTestRule.onNodeWithText("Saved screen").assertIsDisplayed()
   }
 
   @Test
   fun testSettingsDisplayed() {
     composeTestRule.setContent { SettingsScreen() }
     composeTestRule.onNodeWithTag("SettingsScreen").assertIsDisplayed()
+    composeTestRule.onNodeWithText("Settings screen").assertIsDisplayed()
   }
 
   @Test
   fun testUserProfileDisplayed() {
     composeTestRule.setContent { UserProfileScreen() }
     composeTestRule.onNodeWithTag("UserProfileScreen").assertIsDisplayed()
+    composeTestRule.onNodeWithText("User profile screen").assertIsDisplayed()
   }
 
   @Test
   fun testSOmeoneElseUserProfileDisplayed() {
     composeTestRule.setContent { SomeoneElseUserProfileScreen() }
     composeTestRule.onNodeWithTag("SomeoneElseUserProfileScreen").assertIsDisplayed()
+    composeTestRule.onNodeWithText("Someone else user profile screen").assertIsDisplayed()
   }
 }
--- app/src/main/java/com/android/unio/ui/home/Home.kt
+++ app/src/main/java/com/android/unio/ui/home/Home.kt
@@ -10,5 +10,5 @@ import androidx.compose.ui.platform.testTag
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun HomeScreen() {
-  Scaffold(content = { Text("Home Screen") }, modifier = Modifier.testTag("HomeScreen"))
+  Scaffold(content = { Text("Home screen") }, modifier = Modifier.testTag("HomeScreen"))
 }
