d6e0ccc38fccbe5aef07666d5971bdafb8c1967b
armould <arnaud.rajon@epfl.ch>
2024-10-07 18:24:23 +0200

    feat: implement event backend Add an event repository and an event data class, to get and add data to the database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -171,6 +171,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.firebase.common.ktx)
     testImplementation(libs.test.core.ktx)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
---
+++ app/src/main/java/com/android/unio/model/map/Location.kt
@@ -0,0 +1,8 @@
+package com.android.unio.model.map
+
+data class Location(
+    val latitude: Double = 0.0,
+    val longitude: Double = 0.0,
+    val name: String = ""
+)
+
