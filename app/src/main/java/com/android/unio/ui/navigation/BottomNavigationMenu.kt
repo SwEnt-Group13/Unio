75ab149676f5f6633c9db5efc5379738524d1468
Romain Hirschi <romain.hirschi@epfl.ch>
2024-10-08 11:17:48 +0200

    feat(ui/BottomNavigationBar): add tld to the BottomNavigationBar

--- app/src/main/java/com/android/unio/ui/navigation/BottomNavigationMenu.kt
+++ app/src/main/java/com/android/unio/ui/navigation/BottomNavigationMenu.kt
@@ -1,18 +1,31 @@
 package com.android.unio.ui.navigation
 
-import androidx.compose.foundation.layout.Row
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationDefaults
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 
 @Composable
-fun BottomNavigationMenu() {
-
-  BottomNavigation(
-      windowInsets = BottomNavigationDefaults.windowInsets,
-      modifier = Modifier.testTag("BottomNavigationMenu")) {
-        Row {}
-      }
+fun BottomNavigationMenu(
+    onSelection: (TopLevelDestination) -> Unit,
+    tabList: List<TopLevelDestination>,
+    selectedItem: String
+) {
+    BottomNavigation(
+        windowInsets = BottomNavigationDefaults.windowInsets,
+        modifier = Modifier.testTag("bottomNavigationMenu")
+    ) {
+        tabList.map { tld ->
+            BottomNavigationItem(
+                modifier = Modifier.testTag(tld.textId),
+                label = { Text(tld.route) },
+                icon = { Icon(tld.icon, tld.textId) },
+                selected = selectedItem == tld.route,
+                onClick = { onSelection(tld) })
+        }
+    }
 }
