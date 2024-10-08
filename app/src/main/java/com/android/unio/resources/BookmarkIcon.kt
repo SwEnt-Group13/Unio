39bf98edbffc592c0d6fa11a7361c8aab9ec9e27
Romain Hirschi <romain.hirschi@epfl.ch>
2024-10-08 11:02:50 +0200

    docs(resource/BookmarkIcon): add source, license and copyright for BookmarkIcon

--- app/src/main/java/com/android/unio/resources/BookmarkIcon.kt
+++ app/src/main/java/com/android/unio/resources/BookmarkIcon.kt
@@ -1,12 +1,54 @@
-package com.android.unio.resources
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathFillType
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
 
-// Like R, but C
-object C {
-  object Tag {
-    const val greeting = "main_screen_greeting"
-    const val greeting_robo = "second_screen_greeting"
 
-    const val main_screen_container = "main_screen_container"
-    const val second_screen_container = "second_screen_container"
+/*
+From https://composeicons.com/icons/heroicons/bookmark
+Licensed under the MIT license
+Copyright (c) Tailwind Labs, Inc.
+ */
+ val BookmarkIcon: ImageVector
+  get() {
+    if (_Bookmark != null) {
+      return _Bookmark!!
+    }
+    _Bookmark = ImageVector.Builder(
+      name = "Bookmark",
+      defaultWidth = 24.dp,
+      defaultHeight = 24.dp,
+      viewportWidth = 24f,
+      viewportHeight = 24f
+    ).apply {
+      path(
+        fill = null,
+        fillAlpha = 1.0f,
+        stroke = SolidColor(Color(0xFF0F172A)),
+        strokeAlpha = 1.0f,
+        strokeLineWidth = 1.5f,
+        strokeLineCap = StrokeCap.Round,
+        strokeLineJoin = StrokeJoin.Round,
+        strokeLineMiter = 1.0f,
+        pathFillType = PathFillType.NonZero
+      ) {
+        moveTo(17.5933f, 3.32241f)
+        curveTo(18.6939f, 3.4501f, 19.5f, 4.399f, 19.5f, 5.507f)
+        verticalLineTo(21f)
+        lineTo(12f, 17.25f)
+        lineTo(4.5f, 21f)
+        verticalLineTo(5.50699f)
+        curveTo(4.5f, 4.399f, 5.3061f, 3.4501f, 6.4067f, 3.3224f)
+        curveTo(8.2416f, 3.1095f, 10.108f, 3f, 12f, 3f)
+        curveTo(13.892f, 3f, 15.7584f, 3.1095f, 17.5933f, 3.3224f)
+        close()
+      }
+    }.build()
+    return _Bookmark!!
   }
-}
+
+private var _Bookmark: ImageVector? = null
