448d0a5696d346142d3218b83ecebdef2c72e1ce
Aurélien <aurelien.domenget@epfl.ch>
2024-10-08 16:19:59 +0200

    feat(events): add catchy_description, main_type, and picture properties to Event data class

--- app/src/main/java/com/android/unio/model/events/Event.kt
+++ app/src/main/java/com/android/unio/model/events/Event.kt
@@ -3,7 +3,10 @@ package com.android.unio.model.events
 data class Event(
     val id: String,
     val title: String,
+    val catchy_description: String,
     val description: String,
     val date: String, // Consider using Date type with proper formatting
-    val location: String
+    val location: String,
+    val main_type: String,
+    val picture: String
 )
