2085ef34ec5e16d2c032aba94149bb86fb3b9d75
Oskar Codes <oskarbertrand@icloud.com>
2024-10-08 17:42:08 +0200

    chore(Associations): Adapt association data structure to include an url, and default values.

--- app/src/main/java/com/android/unio/model/association/Association.kt
+++ app/src/main/java/com/android/unio/model/association/Association.kt
@@ -2,8 +2,9 @@ package com.android.unio.model.association
 
 data class Association(
     val uid: String,
-    val acronym: String,
-    val fullName: String,
-    val description: String,
-    val members: List<String>
+    val url: String = "",
+    val acronym: String = "",
+    val fullName: String = "",
+    val description: String = "",
+    val members: List<String> = emptyList()
 )
