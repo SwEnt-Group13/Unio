d0b98c7bea8dd9e5632db2f6eb166d680ce08b0d
unknown <romain.hirschi@epfl.ch>
2024-10-09 15:35:11 +0200

    feat(ui/auth): update MainActivity to add welcome/login scrren to navigation

--- app/src/main/java/com/android/unio/MainActivity.kt
+++ app/src/main/java/com/android/unio/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navigation
+import com.android.unio.ui.authentication.LoginScreen
+import com.android.unio.ui.authentication.WelcomeScreen
 import com.android.unio.ui.explore.ExploreScreen
 import com.android.unio.ui.home.HomeScreen
 import com.android.unio.ui.navigation.NavigationAction
@@ -20,31 +22,35 @@ import com.android.unio.ui.saved.SavedScreen
 import com.android.unio.ui.user.UserProfileScreen
 
 class MainActivity : ComponentActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContent { Surface(modifier = Modifier.fillMaxSize()) { UnioApp() } }
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { Surface(modifier = Modifier.fillMaxSize()) { UnioApp() } }
+    }
 }
 
 @Composable
 fun UnioApp() {
-  val navController = rememberNavController()
-  val navigationActions = NavigationAction(navController)
+    val navController = rememberNavController()
+    val navigationActions = NavigationAction(navController)
 
-  // start destination should be Route.AUTH, but for now, we let it be Route.HOME for testing
-  // purposes
-  NavHost(navController = navController, startDestination = Route.HOME) {
-    navigation(startDestination = Screen.HOME, route = Route.HOME) {
-      composable(Screen.HOME) { HomeScreen(navigationActions) }
+    // start destination should be Route.AUTH, but for now, we let it be Route.HOME for testing
+    // purposes
+    NavHost(navController = navController, startDestination = Route.AUTH) {
+        navigation(startDestination = Screen.WELCOME, route = Route.AUTH) {
+            composable(Screen.WELCOME) { WelcomeScreen(navigationActions) }
+            composable(Screen.AUTH) { LoginScreen(navigationActions) }
+        }
+        navigation(startDestination = Screen.HOME, route = Route.HOME) {
+            composable(Screen.HOME) { HomeScreen(navigationActions) }
+        }
+        navigation(startDestination = Screen.EXPLORE, route = Route.EXPLORE) {
+            composable(Screen.EXPLORE) { ExploreScreen(navigationActions) }
+        }
+        navigation(startDestination = Screen.SAVED, route = Route.SAVED) {
+            composable(Screen.SAVED) { SavedScreen(navigationActions) }
+        }
+        navigation(startDestination = Screen.MY_PROFILE, route = Route.MY_PROFILE) {
+            composable(Screen.MY_PROFILE) { UserProfileScreen(navigationActions) }
+        }
     }
-    navigation(startDestination = Screen.EXPLORE, route = Route.EXPLORE) {
-      composable(Screen.EXPLORE) { ExploreScreen(navigationActions) }
-    }
-    navigation(startDestination = Screen.SAVED, route = Route.SAVED) {
-      composable(Screen.SAVED) { SavedScreen(navigationActions) }
-    }
-    navigation(startDestination = Screen.MY_PROFILE, route = Route.MY_PROFILE) {
-      composable(Screen.MY_PROFILE) { UserProfileScreen(navigationActions) }
-    }
-  }
 }
