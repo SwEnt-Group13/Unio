dcbadabed6f9fb05c753efe2143ad872f3baa7f4
unknown <romain.hirschi@epfl.ch>
2024-10-09 15:34:06 +0200

    feat(ui/auth): add welcome and login screen component

--- app/src/main/java/com/android/unio/ui/authentication/Login.kt
+++ app/src/main/java/com/android/unio/ui/authentication/Login.kt
@@ -1,14 +1,49 @@
 package com.android.unio.ui.authentication
 
-import android.annotation.SuppressLint
-import androidx.compose.material.Scaffold
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import com.android.unio.ui.navigation.NavigationAction
+import com.android.unio.ui.navigation.Screen
 
-@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun LoginScreen() {
-  Scaffold(content = { Text("Login screen") }, modifier = Modifier.testTag("LoginScreen"))
+fun LoginScreen(navigationAction: NavigationAction) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    Scaffold(
+        modifier = Modifier
+            .testTag("LoginScreen")
+            .fillMaxSize(),
+        content = { padding ->
+            Column(modifier = Modifier.padding(padding)) {
+                OutlinedTextField(
+                    label = { Text("Enter your university address") },
+                    onValueChange = { email = it },
+                    value = email
+                )
+                OutlinedTextField(
+                    label = { Text("And your password") },
+                    onValueChange = { password = it },
+                    value = password
+                )
+                Button(onClick = {
+                    /* TODO Handle login with Tequila/Microsoft if possible */
+                    navigationAction.navigateTo(Screen.HOME)
+                }) {
+                    Text("Continue")
+                }
+            }
+        }
+    )
 }
--- app/src/main/java/com/android/unio/ui/authentication/WelcomeScreen.kt
+++ app/src/main/java/com/android/unio/ui/authentication/WelcomeScreen.kt
@@ -1,8 +1,39 @@
 package com.android.unio.ui.authentication
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.android.unio.R
+import com.android.unio.ui.navigation.NavigationAction
+import com.android.unio.ui.navigation.Screen
 
 @Composable
-fun WelcomeScreen() {
-
+fun WelcomeScreen(navigationAction: NavigationAction) {
+    Scaffold(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.padding(it)) {
+            Image(
+                //Placeholder for the Unio logo
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = "Unio logo"
+            )
+            Text("The world’s largest campus life platform !")
+            Button(
+                onClick = { navigationAction.navigateTo(Screen.AUTH) }
+            ) {
+                Text("Login")
+            }
+            Button(
+                onClick = { navigationAction.navigateTo(Screen.HOME) }
+            ) {
+                Text("<Debug> Skip to Home")
+            }
+        }
+    }
 }
