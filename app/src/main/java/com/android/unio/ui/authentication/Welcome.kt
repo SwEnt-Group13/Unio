1194f972710b1e79d2bba5d9fc74490617bb18e8
Romain Hirschi <romain.hirschi@epfl.ch>
2024-10-10 10:52:23 +0200

    style: format code with ktfmt

--- app/src/main/java/com/android/unio/ui/authentication/Welcome.kt
+++ app/src/main/java/com/android/unio/ui/authentication/Welcome.kt
@@ -1,6 +1,5 @@
 package com.android.unio.ui.authentication
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,10 +11,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.android.unio.R
 import com.android.unio.ui.navigation.NavigationAction
 import com.android.unio.ui.navigation.Screen
 
@@ -26,11 +23,11 @@ fun WelcomeScreen(navigationAction: NavigationAction) {
         modifier = Modifier.padding(it).padding(vertical = 200.dp, horizontal = 100.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally) {
-//          Image(
-//              // Placeholder for the Unio logo
-//              painter = painterResource(id = R.drawable.ic_launcher_foreground),
-//              contentDescription = "Unio logo",
-//              modifier = Modifier.padding(16.dp))
+          //          Image(
+          //              // Placeholder for the Unio logo
+          //              painter = painterResource(id = R.drawable.ic_launcher_foreground),
+          //              contentDescription = "Unio logo",
+          //              modifier = Modifier.padding(16.dp))
           Text(
               "The world’s largest campus life platform !",
               textAlign = TextAlign.Center,
