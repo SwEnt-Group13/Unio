bbc5d8477b64fed66ce15a5360b6336b007e0710
armould <arnaud.rajon@epfl.ch>
2024-10-11 13:52:15 +0200

    fix(model/image): upload image Fix uploadImage so it outputs the image's download link automatically.

--- app/src/main/java/com/android/unio/model/image/ImageRepository.kt
+++ app/src/main/java/com/android/unio/model/image/ImageRepository.kt
@@ -1,15 +1,13 @@
 package com.android.unio.model.image
 
-import com.google.firebase.storage.UploadTask.TaskSnapshot
 import java.io.InputStream
 
 interface ImageRepository {
-  fun getImageUrl(firebasePath: String, onSuccess: (String) -> Unit, onFailure: (Exception) -> Unit)
 
   fun uploadImage(
       imageStream: InputStream,
       firebasePath: String,
-      onSuccess: (TaskSnapshot) -> Unit,
+      onSuccess: (String) -> Unit,
       onFailure: (Exception) -> Unit
   )
 }
