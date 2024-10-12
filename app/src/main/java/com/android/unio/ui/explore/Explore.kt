75cca77c18403298a57870395d0bc33df7032239
Zafar <isadjanovzafar@gmail.com>
2024-10-12 16:33:10 +0200

    chore(explore): adapt explore screen to figma design
    This adapts the visual design as well as the behavioral design according to the one in the figma.
    - Added the screen title
    - Added a search bar with the proper design (without the filtering)
    - Changed and adapted most of the styles and modifiers

--- app/src/main/java/com/android/unio/ui/explore/Explore.kt
+++ app/src/main/java/com/android/unio/ui/explore/Explore.kt
@@ -1,9 +1,8 @@
 package com.android.unio.ui.explore
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
@@ -12,22 +11,29 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.android.unio.R
 import com.android.unio.model.association.Association
 import com.android.unio.model.association.AssociationType
 import com.android.unio.model.association.MockAssociation
@@ -50,71 +56,91 @@ fun ExploreScreen(navigationAction: NavigationAction) {
       content = { padding -> ExploreScreenContent(padding, navigationAction) })
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ExploreScreenContent(padding: PaddingValues, navigationAction: NavigationAction) {
-  // This is a placeholder for the actual content of the Explore screen
-  Column(modifier = Modifier.fillMaxSize().padding(padding)) {
-    OutlinedTextField(
-        value = "",
-        onValueChange = {},
-        modifier = Modifier.fillMaxWidth().padding(8.dp).testTag("searchBar"),
-        placeholder = { Text("Search") },
-        trailingIcon = { Icon(Icons.Default.Search, contentDescription = "Search icon") })
+  val searchQuery = remember { mutableStateOf("") }
+  Column(modifier = Modifier.padding(padding)) {
+    Text(
+        text = "Explore our Associations",
+        /** Will go in the string.xml */
+        style = MaterialTheme.typography.headlineLarge,
+        modifier = Modifier.padding(horizontal = 29.dp, vertical = 16.dp).testTag("exploreTitle"))
+
+    SearchBar(
+        inputField = {
+          SearchBarDefaults.InputField(
+              modifier = Modifier.testTag("searchBarInput"),
+              query = searchQuery.value,
+              onQueryChange = { searchQuery.value = it },
+              onSearch = { /* Handle search here */},
+              expanded = false,
+              onExpandedChange = { /* Handle expanded state change here */},
+              placeholder = { Text("Search") },
+              trailingIcon = {
+                IconButton(onClick = {}) {
+                  Icon(Icons.Default.Search, contentDescription = "Search icon")
+                }
+              },
+          )
+        },
+        expanded = false,
+        onExpandedChange = { /* Also handle expanded state change here */},
+        modifier = Modifier.padding(horizontal = 26.dp).testTag("searchBar"),
+        content = {},
+    )
 
     LazyColumn(
-        modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.spacedBy(16.dp)) {
-          AssociationType.entries.forEach { category ->
-            val filteredAssociations =
-                getFilteredAssociationsByCategoryAndAlphabeticalOrder(category)
+        modifier = Modifier.fillMaxSize().testTag("categoriesList"),
+        contentPadding = PaddingValues(vertical = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+    ) {
+      AssociationType.entries.forEach { category ->
+        val filteredAssociations = getFilteredAssociationsByCategoryAndAlphabeticalOrder(category)
 
-            if (filteredAssociations.isNotEmpty()) {
-              item {
-                Text(
-                    text = getCategoryNameWithFirstLetterUppercase(category),
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(horizontal = 16.dp).testTag("categoryTitle"))
+        if (filteredAssociations.isNotEmpty()) {
+          item {
+            Text(
+                text = getCategoryNameWithFirstLetterUppercase(category),
+                style = MaterialTheme.typography.headlineSmall,
+                modifier = Modifier.padding(horizontal = 16.dp))
 
-                // Horizontal scrollable list of associations
-                LazyRow(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.spacedBy(16.dp)) {
-                      items(filteredAssociations.size) { index ->
-                        AssociationItem(filteredAssociations[index].association, navigationAction)
-                      }
-                    }
-              }
-            }
+            // Horizontal scrollable list of associations
+            LazyRow(
+                modifier = Modifier.fillMaxSize().padding(vertical = 16.dp),
+                contentPadding = PaddingValues(horizontal = 16.dp),
+                horizontalArrangement = Arrangement.spacedBy(32.dp, Alignment.Start),
+                verticalAlignment = Alignment.CenterVertically) {
+                  items(filteredAssociations.size) { index ->
+                    AssociationItem(filteredAssociations[index].association, navigationAction)
+                  }
+                }
           }
         }
+      }
+    }
   }
 }
 
 @Composable
 fun AssociationItem(association: Association, navigationAction: NavigationAction) {
-  Column(
-      modifier =
-          Modifier.padding(16.dp).width(80.dp).height(120.dp).clickable {
-            navigationAction.navigateTo(Screen.ASSOCIATION)
-          }
-      // Interaction (to see detailed screen about an association) can be defined here,
-      // with the .clickable modifier
-      ) {
-        /**
-         * Placeholder for the image later on when we find a way to get and store them, for now just
-         * a gray box
-         */
-        Box(
-            modifier =
-                Modifier.size(64.dp).background(Color.LightGray, shape = RoundedCornerShape(8.dp)))
+  Column(modifier = Modifier.clickable { navigationAction.navigateTo(Screen.ASSOCIATION) }) {
+    /**
+     * AdEC image is used as the placeholder. Will need to add the actual image later, when the
+     * actual view model is used.
+     */
+    Image(
+        painter = painterResource(id = R.drawable.adec),
+        contentDescription = "image description",
+        modifier = Modifier.size(124.dp))
 
-        Spacer(modifier = Modifier.height(8.dp))
+    Spacer(modifier = Modifier.height(8.dp))
 
-        Text(
-            text = association.acronym,
-            fontWeight = FontWeight.Normal,
-            modifier = Modifier.fillMaxWidth(),
-            maxLines = 1)
-      }
+    Text(
+        text = association.acronym,
+        style = MaterialTheme.typography.bodyMedium,
+        modifier = Modifier.fillMaxWidth().align(Alignment.CenterHorizontally))
+  }
 }
 
 /** Returns a list of associations filtered by the given category. */
