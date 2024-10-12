f1122d57606ace022c02e8c542f79a52face0f17
Oskar Codes <oskarbertrand@icloud.com>
2024-10-12 15:08:57 +0200

    feat(auth): Create Authentication logic to sign in/up, with appropriate error handling
    The error handling may need to be enhanced, to include every possible Firebase Auth error case.

---
+++ app/src/main/java/com/android/unio/model/user/Auth.kt
@@ -0,0 +1,48 @@
+package com.android.unio.model.user
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseUser
+
+enum class SignInState {
+  INVALID_CREDENTIALS,
+  INVALID_EMAIL_FORMAT,
+  SUCCESS_SIGN_IN,
+  SUCCESS_CREATE_ACCOUNT
+}
+
+data class SignInResult(val state: SignInState, val user: FirebaseUser?)
+
+fun signInOrCreateAccount(
+    email: String,
+    password: String,
+    auth: FirebaseAuth,
+    onResult: (SignInResult) -> Unit,
+) {
+  if (isValidEmail(email)) {
+    auth
+        .signInWithEmailAndPassword(email, password)
+        .addOnSuccessListener { onResult(SignInResult(SignInState.SUCCESS_SIGN_IN, it.user)) }
+        .addOnFailureListener {
+          println(it)
+          if (it is FirebaseAuthInvalidCredentialsException) {
+            auth
+                .createUserWithEmailAndPassword(email, password)
+                .addOnSuccessListener {
+                  onResult(SignInResult(SignInState.SUCCESS_CREATE_ACCOUNT, it.user))
+                }
+                .addOnFailureListener {
+                  onResult(SignInResult(SignInState.INVALID_CREDENTIALS, null))
+                }
+          } else {
+            onResult(SignInResult(SignInState.INVALID_CREDENTIALS, null))
+          }
+        }
+  } else {
+    onResult(SignInResult(SignInState.INVALID_EMAIL_FORMAT, null))
+  }
+}
+
+fun isValidEmail(text: String): Boolean {
+  return text.trim().isNotEmpty() && text.trim().matches(Regex("^.+@.+\\..+$"))
+}
