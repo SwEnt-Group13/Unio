84dfeaf4778e23db8764a386a64f11cb62abbe30
Oskar Codes <oskarbertrand@icloud.com>
2024-10-13 17:18:57 +0200

    chore(scrape): Create a scraping script to get EPFL-recognized associations by category.

---
+++ misc/scrape-categories.js
@@ -0,0 +1,29 @@
+// Run the following code on https://www.epfl.ch/campus/associations/category/
+// to extract the list of associations by category.
+
+(() => {
+  const categoriesHeaders = Array.from(document.querySelectorAll("#post-2124 > div > h3"));
+  
+  const categories = categoriesHeaders.map((e) => {
+    const categoryName = e.textContent;
+    const associationList = Array.from(e.nextElementSibling.children).map((e) => {
+      const parts = e.textContent.split("–");
+      const acronym = parts[0]?.trim();
+      const name = parts[1]?.trim();
+      const url = e.querySelector("a")?.href;
+  
+      return {
+        acronym,
+        name,
+        url
+      };
+    });
+  
+    return {
+      categoryName,
+      associationList
+    };
+  });
+  
+  console.log(JSON.stringify(categories));
+})();
