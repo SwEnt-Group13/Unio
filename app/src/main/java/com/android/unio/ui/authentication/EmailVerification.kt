4a9e7e398bb9404bef472b1db87ff74f34c8072a
Oskar Codes <oskarbertrand@icloud.com>
2024-10-13 13:54:27 +0200

    feat(EmailVerification): Add a TopAppBar to the EmailVerificationScreen so that users can go back.

--- app/src/main/java/com/android/unio/ui/authentication/EmailVerification.kt
+++ app/src/main/java/com/android/unio/ui/authentication/EmailVerification.kt
@@ -10,12 +10,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ArrowBack
 import androidx.compose.material.icons.outlined.Refresh
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -33,6 +37,7 @@ import com.android.unio.ui.theme.AppTypography
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EmailVerificationScreen(navigationAction: NavigationAction) {
 
@@ -67,6 +72,17 @@ fun EmailVerificationScreen(navigationAction: NavigationAction) {
 
   Scaffold(
       modifier = Modifier.testTag("EmailVerificationScreen").fillMaxSize(),
+      topBar = {
+        TopAppBar(
+            title = {},
+            navigationIcon = {
+              IconButton(onClick = { Firebase.auth.signOut() }) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Outlined.ArrowBack,
+                    contentDescription = "Go back")
+              }
+            })
+      },
       content = { padding ->
         Column(
             modifier = Modifier.padding(padding).fillMaxSize(),
