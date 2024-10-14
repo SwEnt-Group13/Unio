b4690766db441910703821f3fa9f69c2ad281dca
Oskar Codes <oskarbertrand@icloud.com>
2024-10-14 20:03:25 +0200

    refactor(serialization): Extract serialization and hydration methods of Firestore repositories to extension functions.

---
+++ app/src/main/java/com/android/unio/model/firestore/transform/Hydration.kt
@@ -0,0 +1,83 @@
+package com.android.unio.model.firestore.transform
+
+import com.android.unio.model.association.Association
+import com.android.unio.model.association.AssociationRepositoryFirestore
+import com.android.unio.model.event.Event
+import com.android.unio.model.event.EventRepositoryFirestore
+import com.android.unio.model.event.EventType
+import com.android.unio.model.firestore.FirestorePaths.ASSOCIATION_PATH
+import com.android.unio.model.firestore.FirestorePaths.USER_PATH
+import com.android.unio.model.firestore.FirestoreReferenceList
+import com.android.unio.model.map.Location
+import com.android.unio.model.user.User
+import com.android.unio.model.user.UserRepositoryFirestore
+import com.google.firebase.Firebase
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.firestore
+
+fun AssociationRepositoryFirestore.Companion.hydrate(data: Map<String, Any>?): Association {
+  val memberUids = data?.get("members") as? List<String> ?: emptyList()
+  val members =
+      FirestoreReferenceList.fromList(
+          list = memberUids,
+          collection = Firebase.firestore.collection(USER_PATH),
+          hydrate = UserRepositoryFirestore::hydrate)
+
+  return Association(
+      uid = data?.get("uid") as? String ?: "",
+      url = data?.get("url") as? String ?: "",
+      acronym = data?.get("acronym") as? String ?: "",
+      fullName = data?.get("fullName") as? String ?: "",
+      description = data?.get("description") as? String ?: "",
+      members = members)
+}
+
+fun UserRepositoryFirestore.Companion.hydrate(data: Map<String, Any>?): User {
+  val followingAssociationsUids = data?.get("followingAssociations") as? List<String> ?: emptyList()
+  val followingAssociations =
+      FirestoreReferenceList.fromList(
+          followingAssociationsUids,
+          Firebase.firestore.collection(ASSOCIATION_PATH),
+          AssociationRepositoryFirestore::hydrate)
+
+  return User(
+      uid = data?.get("uid") as? String ?: "",
+      name = data?.get("name") as? String ?: "",
+      email = data?.get("email") as? String ?: "",
+      followingAssociations = followingAssociations)
+}
+
+fun EventRepositoryFirestore.Companion.hydrate(data: Map<String, Any>?): Event {
+  val organisers =
+      FirestoreReferenceList.fromList(
+          data?.get("organisers") as? List<String> ?: emptyList(),
+          Firebase.firestore.collection(ASSOCIATION_PATH),
+          AssociationRepositoryFirestore::hydrate)
+
+  val taggedAssociations =
+      FirestoreReferenceList.fromList(
+          data?.get("taggedAssociations") as? List<String> ?: emptyList(),
+          Firebase.firestore.collection(ASSOCIATION_PATH),
+          AssociationRepositoryFirestore::hydrate)
+
+  val types = (data?.get("types") as? List<String> ?: emptyList())
+
+  val location = data?.get("location") as? Map<String, Any>
+
+  return Event(
+      uid = data?.get("uid") as? String ?: "",
+      title = data?.get("title") as? String ?: "",
+      organisers = organisers,
+      taggedAssociations = taggedAssociations,
+      image = data?.get("image") as? String ?: "",
+      description = data?.get("description") as? String ?: "",
+      catchyDescription = data?.get("catchyDescription") as? String ?: "",
+      price = data?.get("price") as? Double ?: 0.0,
+      date = data?.get("date") as? Timestamp ?: Timestamp.now(),
+      location =
+          Location(
+              latitude = location?.get("latitude") as? Double ?: 0.0,
+              longitude = location?.get("longitude") as? Double ?: 0.0,
+              name = location?.get("name") as? String ?: ""),
+      types = types.map { EventType.valueOf(it) })
+}
---
+++ app/src/main/java/com/android/unio/model/firestore/transform/Serialization.kt
@@ -0,0 +1,45 @@
+package com.android.unio.model.firestore.transform
+
+import com.android.unio.model.association.Association
+import com.android.unio.model.association.AssociationRepositoryFirestore
+import com.android.unio.model.event.Event
+import com.android.unio.model.event.EventRepositoryFirestore
+import com.android.unio.model.user.User
+import com.android.unio.model.user.UserRepositoryFirestore
+
+fun AssociationRepositoryFirestore.Companion.serialize(association: Association): Map<String, Any> {
+  return mapOf(
+      "uid" to association.uid,
+      "url" to association.url,
+      "acronym" to association.acronym,
+      "fullName" to association.fullName,
+      "description" to association.description,
+      "members" to association.members.list.value.map { it.uid })
+}
+
+fun UserRepositoryFirestore.Companion.serialize(user: User): Map<String, Any> {
+  return mapOf(
+      "uid" to user.uid,
+      "name" to user.name,
+      "email" to user.email,
+      "followingAssociations" to user.followingAssociations.list.value.map { it.uid })
+}
+
+fun EventRepositoryFirestore.Companion.serialize(event: Event): Map<String, Any> {
+  return mapOf(
+      "uid" to event.uid,
+      "title" to event.title,
+      "organisers" to event.organisers.list.value.map { it.uid },
+      "taggedAssociations" to event.taggedAssociations.list.value.map { it.uid },
+      "image" to event.image,
+      "description" to event.description,
+      "catchyDescription" to event.catchyDescription,
+      "price" to event.price,
+      "date" to event.date,
+      "location" to
+          mapOf(
+              "latitude" to event.location.latitude,
+              "longitude" to event.location.longitude,
+              "name" to event.location.name),
+      "types" to event.types.map { it.name })
+}
