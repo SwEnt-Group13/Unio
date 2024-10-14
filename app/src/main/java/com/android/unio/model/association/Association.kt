bd624dfdf93a45a6750a294dfbdaea7e698f3c7d
Oskar Codes <oskarbertrand@icloud.com>
2024-10-14 20:06:15 +0200

    chore(comments): Add comments to Association, Event and User.

--- app/src/main/java/com/android/unio/model/association/Association.kt
+++ app/src/main/java/com/android/unio/model/association/Association.kt
@@ -3,6 +3,17 @@ package com.android.unio.model.association
 import com.android.unio.model.firestore.ReferenceList
 import com.android.unio.model.user.User
 
+/**
+ * Association data class Make sure to update the hydration and serialization methods when changing
+ * the data class
+ *
+ * @property uid association id
+ * @property url association url
+ * @property acronym association acronym
+ * @property fullName association full name
+ * @property description association description
+ * @property members list of association members
+ */
 data class Association(
     val uid: String,
     val url: String = "",
--- app/src/main/java/com/android/unio/model/user/User.kt
+++ app/src/main/java/com/android/unio/model/user/User.kt
@@ -3,6 +3,15 @@ package com.android.unio.model.user
 import com.android.unio.model.association.Association
 import com.android.unio.model.firestore.ReferenceList
 
+/**
+ * User data class Make sure to update the hydration and serialization methods when changing the
+ * data class
+ *
+ * @property uid user id
+ * @property name user name
+ * @property email user email
+ * @property followingAssociations list of associations that the user is following
+ */
 data class User(
     val uid: String,
     val name: String,
