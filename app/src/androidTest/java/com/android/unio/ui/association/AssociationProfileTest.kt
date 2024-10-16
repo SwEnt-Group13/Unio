d58bd15691196abda8951d45f86bddd5ef46e454
Oskar Codes <oskarbertrand@icloud.com>
2024-10-16 14:43:59 +0200

    chore(FirestoreReferenceList): Use newly created helper function in the AssociationProfileTest.

--- app/src/androidTest/java/com/android/unio/ui/association/AssociationProfileTest.kt
+++ app/src/androidTest/java/com/android/unio/ui/association/AssociationProfileTest.kt
@@ -10,10 +10,7 @@ import androidx.navigation.NavHostController
 import com.android.unio.model.association.Association
 import com.android.unio.model.association.AssociationRepository
 import com.android.unio.model.association.AssociationViewModel
-import com.android.unio.model.firestore.FirestorePaths.USER_PATH
-import com.android.unio.model.firestore.FirestoreReferenceList
-import com.android.unio.model.firestore.transform.hydrate
-import com.android.unio.model.user.UserRepositoryFirestore
+import com.android.unio.model.user.User
 import com.android.unio.ui.navigation.NavigationAction
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
@@ -54,9 +51,7 @@ class AssociationProfileTest {
                 fullName = "Association for Computing Machinery",
                 description =
                     "ACM is the world's largest educational and scientific computing society.",
-                members =
-                    FirestoreReferenceList.empty(
-                        db.collection(USER_PATH), UserRepositoryFirestore.Companion::hydrate)))
+                members = User.emptyFirestoreReferenceList()))
 
     navHostController = mock { NavHostController::class.java }
     navigationAction = NavigationAction(navHostController)
