f0c7ddccfc04204fed05987aeda5eb395a011348
Redd87 <eddy.p.rashed@gmail.com>
2024-10-17 10:13:53 +0200

    feat(association): Add partial AssociationDocument and conversion function for AppSearch integration

--- app/src/main/java/com/android/unio/model/association/Association.kt
+++ app/src/main/java/com/android/unio/model/association/Association.kt
@@ -1,5 +1,10 @@
 package com.android.unio.model.association
 
+import androidx.appsearch.annotation.Document
+import androidx.appsearch.annotation.Document.Id
+import androidx.appsearch.annotation.Document.Namespace
+import androidx.appsearch.annotation.Document.StringProperty
+import androidx.appsearch.app.AppSearchSchema.StringPropertyConfig
 import com.android.unio.model.firestore.ReferenceList
 import com.android.unio.model.user.User
 
@@ -11,3 +16,27 @@ data class Association(
     val description: String = "",
     val members: ReferenceList<User>
 )
+
+@Document
+data class AssociationDocument(
+    @Namespace val namespace: String = "",
+    @Id val uid: String,
+    @StringProperty(indexingType = StringPropertyConfig.INDEXING_TYPE_PREFIXES)
+    val url: String = "",
+    @StringProperty(indexingType = StringPropertyConfig.INDEXING_TYPE_PREFIXES)
+    val acronym: String = "",
+    @StringProperty(indexingType = StringPropertyConfig.INDEXING_TYPE_PREFIXES)
+    val fullName: String = "",
+    @StringProperty(indexingType = StringPropertyConfig.INDEXING_TYPE_PREFIXES)
+    val description: String = ""
+    // TODO add members
+)
+
+fun Association.toAssociationDocument(): AssociationDocument {
+  return AssociationDocument(
+      uid = this.uid,
+      url = this.url,
+      acronym = this.acronym,
+      fullName = this.fullName,
+      description = this.description)
+}
