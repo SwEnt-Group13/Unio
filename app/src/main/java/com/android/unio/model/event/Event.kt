af9923a7d3de9945d3b6b8712d6c9ed32551374b
Redd87 <eddy.p.rashed@gmail.com>
2024-10-17 10:14:21 +0200

    feat(event): Add partial EventDocument and conversion function for AppSearch integration

--- app/src/main/java/com/android/unio/model/event/Event.kt
+++ app/src/main/java/com/android/unio/model/event/Event.kt
@@ -1,5 +1,10 @@
 package com.android.unio.model.event
 
+import androidx.appsearch.annotation.Document
+import androidx.appsearch.annotation.Document.Id
+import androidx.appsearch.annotation.Document.Namespace
+import androidx.appsearch.annotation.Document.StringProperty
+import androidx.appsearch.app.AppSearchSchema.StringPropertyConfig
 import com.android.unio.model.association.Association
 import com.android.unio.model.firestore.ReferenceList
 import com.android.unio.model.map.Location
@@ -19,3 +24,29 @@ data class Event(
     val location: Location = Location(),
     val types: List<EventType> = mutableListOf<EventType>()
 )
+
+@Document
+data class EventDocument(
+    @Namespace val namespace: String = "",
+    @Id val uid: String = "",
+    @StringProperty(indexingType = StringPropertyConfig.INDEXING_TYPE_PREFIXES)
+    val title: String = "",
+    @StringProperty(indexingType = StringPropertyConfig.INDEXING_TYPE_PREFIXES)
+    val description: String,
+    @StringProperty(indexingType = StringPropertyConfig.INDEXING_TYPE_PREFIXES)
+    val catchyDescription: String = "",
+    @StringProperty(indexingType = StringPropertyConfig.INDEXING_TYPE_PREFIXES)
+    val locationName: String = ""
+    //TODO Need to add organisers and taggedAssociations
+)
+
+fun Event.toEventDocument(): EventDocument {
+  return EventDocument(
+      uid = this.uid,
+      title = this.title,
+      description = this.description,
+      catchyDescription = this.catchyDescription,
+      locationName = this.location.name
+      // Include other fields as needed
+      )
+}
