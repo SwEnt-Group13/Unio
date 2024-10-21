24a31f843220e53a674be00683744dfdb418f073
Alexei Thornber <AlexeiThornber@gmail.com>
2024-10-21 12:52:23 +0200

    style(MainActivity): ktfmt

--- app/src/main/java/com/android/unio/MainActivity.kt
+++ app/src/main/java/com/android/unio/MainActivity.kt
@@ -59,16 +59,19 @@ fun UnioApp() {
     val user = auth.currentUser
     if (user != null) {
       if (user.isEmailVerified) {
-        userRepositoryFirestore.getUserWithId(user.uid,{
-          if(it.hasProvidedAccountDetails){
-            navigationActions.navigateTo(Screen.HOME)
-          } else {
-            navigationActions.navigateTo(Screen.ACCOUNT_DETAILS)
-          }
-        },{
-            Log.e("UnioApp", "Error fetching account details: $it")
-            Toast.makeText(context, "Error fetching account details.", Toast.LENGTH_SHORT).show()
-        })
+        userRepositoryFirestore.getUserWithId(
+            user.uid,
+            {
+              if (it.hasProvidedAccountDetails) {
+                navigationActions.navigateTo(Screen.HOME)
+              } else {
+                navigationActions.navigateTo(Screen.ACCOUNT_DETAILS)
+              }
+            },
+            {
+              Log.e("UnioApp", "Error fetching account details: $it")
+              Toast.makeText(context, "Error fetching account details.", Toast.LENGTH_SHORT).show()
+            })
       } else {
         navigationActions.navigateTo(Screen.EMAIL_VERIFICATION)
       }
