d56fa26f2899833374e0babbf1418ee3e993dd9f
Zafar <isadjanovzafar@gmail.com>
2024-10-25 21:18:40 +0200

    feat(map): implement simple map showing event markers
    These changes simply introduce the usage of Google maps to display the events that are fetched through the event view model. No color yet.

--- app/src/main/java/com/android/unio/ui/map/Map.kt
+++ app/src/main/java/com/android/unio/ui/map/Map.kt
@@ -1,14 +1,74 @@
 package com.android.unio.ui.map
 
-import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.android.unio.R
+import com.android.unio.model.event.EventListViewModel
+import com.android.unio.resources.ResourceManager.getString
+import com.android.unio.ui.navigation.NavigationAction
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MapScreen() {
-  Scaffold(modifier = Modifier.testTag("MapScreen")) { Text("Map screen") }
+fun MapScreen(navigationAction: NavigationAction) {
+  Scaffold(
+      modifier = Modifier.testTag("MapScreen"),
+      topBar = {
+        TopAppBar(
+            title = { Text("Event map", modifier = Modifier.testTag("MapTitle")) },
+            navigationIcon = {
+              IconButton(
+                  onClick = { navigationAction.goBack() },
+                  modifier = Modifier.testTag("goBackButton")) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Outlined.ArrowBack,
+                        contentDescription = getString(R.string.association_go_back))
+                  }
+            })
+      }) { pd ->
+        EventMap(pd)
+      }
+}
+
+@Composable
+fun EventMap(
+    pd: PaddingValues,
+    eventListViewModel: EventListViewModel = viewModel(factory = EventListViewModel.Factory)
+) {
+  val cameraPositionState = rememberCameraPositionState {
+    position = CameraPosition.fromLatLngZoom(LatLng(46.518831258, 6.559331096), 10f)
+  }
+
+  val events = eventListViewModel.events.collectAsState()
+
+  GoogleMap(
+      modifier = Modifier.padding(pd).testTag("googleMaps"),
+      cameraPositionState = cameraPositionState) {
+        events.value.forEach { event ->
+          event.location.let {
+            Marker(
+                state = MarkerState(position = LatLng(it.latitude, it.longitude)),
+                title = event.title,
+                snippet = event.description)
+          }
+        }
+      }
 }
