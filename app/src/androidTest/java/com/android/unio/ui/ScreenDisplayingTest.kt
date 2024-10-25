6e07b7e94718ccfa23bd2961dc2996f5df9656b4
Zafar <isadjanovzafar@gmail.com>
2024-10-25 21:31:21 +0200

    chore(test): adjust ScreenDisplayingTest.kt to new MapScreen parameter

--- app/src/androidTest/java/com/android/unio/ui/ScreenDisplayingTest.kt
+++ app/src/androidTest/java/com/android/unio/ui/ScreenDisplayingTest.kt
@@ -91,7 +91,7 @@ class ScreenDisplayingTest {
 
   @Test
   fun testMapDisplayed() {
-    composeTestRule.setContent { MapScreen() }
+    composeTestRule.setContent { MapScreen(navigationAction) }
     composeTestRule.onNodeWithTag("MapScreen").assertIsDisplayed()
   }
 
