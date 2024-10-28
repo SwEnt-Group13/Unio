68b165bcbad25d95e39239024e4fceeaf7d46e48
Zafar <isadjanovzafar@gmail.com>
2024-10-28 23:07:32 +0100

    feat(map): add saved event display on the map with a custom pin point
    - Added a custom pin point that refers to a "favorited" event
    - Added a mock list of an arbitrary saved event list. This will simply be replaced by the list of the saved events by the user when it is available (waiting for another PR)
    - Changed the color of the pin points that correspond to all the events to blue. This will later on be refactored to only take events that are in the very near future.

--- app/src/main/java/com/android/unio/ui/map/Map.kt
+++ app/src/main/java/com/android/unio/ui/map/Map.kt
@@ -1,5 +1,7 @@
 package com.android.unio.ui.map
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -13,18 +15,26 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.android.unio.R
+import com.android.unio.model.association.Association
+import com.android.unio.model.event.Event
 import com.android.unio.model.event.EventListViewModel
+import com.android.unio.model.firestore.emptyFirestoreReferenceList
+import com.android.unio.model.map.Location
 import com.android.unio.resources.ResourceManager.getString
 import com.android.unio.ui.navigation.NavigationAction
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.firebase.Timestamp
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
+import java.util.Calendar
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -59,15 +69,53 @@ fun EventMap(pd: PaddingValues, eventListViewModel: EventListViewModel) {
 
   val events = eventListViewModel.events.collectAsState()
 
+  /** Mock arbitrary saved event for all users. */
+  val arbitrarySavedEvents: List<Event> =
+      listOf(
+          Event(
+              uid = "123456789",
+              title = "Arbitrary Saved Event",
+              organisers = Association.emptyFirestoreReferenceList(),
+              taggedAssociations = Association.emptyFirestoreReferenceList(),
+              description = "This is the description of an arbitrary saved event.",
+              catchyDescription = "This is an arbitrary saved event.",
+              date =
+                  Timestamp(
+                      Calendar.getInstance()
+                          .apply { add(Calendar.DAY_OF_YEAR, 1) }
+                          .time), // tomorrow's date
+              location =
+                  Location(
+                      latitude = 46.51848436506024, longitude = 6.568259761045008), // Rolex Center
+          ))
+
   GoogleMap(
       modifier = Modifier.padding(pd).testTag("googleMaps"),
       cameraPositionState = cameraPositionState) {
+        // Display saved events
+        arbitrarySavedEvents.forEach { event ->
+          event.location.let {
+            val bitmap =
+                BitmapFactory.decodeResource(LocalContext.current.resources, R.drawable.favorite_pinpoint)
+            val scaledBitmap = Bitmap.createScaledBitmap(bitmap, 64, 64, false)
+            val customIcon = BitmapDescriptorFactory.fromBitmap(scaledBitmap)
+            Marker(
+                state = MarkerState(position = LatLng(it.latitude, it.longitude)),
+                title = event.title,
+                snippet = event.description,
+                icon = customIcon)
+          }
+        }
+
+        // Display all events (should refactor to the ones that are soon to happen when more events
+        // are added)
         events.value.forEach { event ->
           event.location.let {
             Marker(
                 state = MarkerState(position = LatLng(it.latitude, it.longitude)),
                 title = event.title,
-                snippet = event.description)
+                snippet = event.description,
+                icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
           }
         }
       }
