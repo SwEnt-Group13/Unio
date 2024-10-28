132c40551e2f7719d51ce6092fc9fcafbfab60d6
Romain Hirschi <romain.hirschi@epfl.ch>
2024-10-28 11:23:58 +0100

    fix(model/Hydratation): add hydratation behavior for follower count

--- app/src/main/java/com/android/unio/model/firestore/transform/Hydration.kt
+++ app/src/main/java/com/android/unio/model/firestore/transform/Hydration.kt
@@ -16,75 +16,82 @@ import com.android.unio.model.user.UserSocial
 import com.google.firebase.Timestamp
 
 fun AssociationRepositoryFirestore.Companion.hydrate(data: Map<String, Any>?): Association {
-  val category = data?.get("category")
-  val memberUids = data?.get("members") as? List<String> ?: emptyList()
-  val members = User.firestoreReferenceListWith(memberUids)
+    val category = data?.get("category")
+    val memberUids = data?.get("members") as? List<String> ?: emptyList()
+    val members = User.firestoreReferenceListWith(memberUids)
 
-  return Association(
-      uid = data?.get("uid") as? String ?: "",
-      url = data?.get("url") as? String ?: "",
-      name = data?.get("name") as? String ?: "",
-      fullName = data?.get("fullName") as? String ?: "",
-      category =
-          if (category is String) AssociationCategory.valueOf(category)
-          else AssociationCategory.UNKNOWN,
-      description = data?.get("description") as? String ?: "",
-      members = members,
-      image = data?.get("image") as? String ?: "")
+    return Association(
+        uid = data?.get("uid") as? String ?: "",
+        url = data?.get("url") as? String ?: "",
+        name = data?.get("name") as? String ?: "",
+        fullName = data?.get("fullName") as? String ?: "",
+        category =
+        if (category is String) AssociationCategory.valueOf(category)
+        else AssociationCategory.UNKNOWN,
+        description = data?.get("description") as? String ?: "",
+        members = members,
+        followersCount = data?.get("followersCount") as? Int ?: 0,
+        image = data?.get("image") as? String ?: ""
+    )
 }
 
 fun UserRepositoryFirestore.Companion.hydrate(data: Map<String, Any>?): User {
-  val followingAssociationsUids = data?.get("followingAssociations") as? List<String> ?: emptyList()
-  val followingAssociations = Association.firestoreReferenceListWith(followingAssociationsUids)
+    val followingAssociationsUids =
+        data?.get("followingAssociations") as? List<String> ?: emptyList()
+    val followingAssociations = Association.firestoreReferenceListWith(followingAssociationsUids)
 
-  val joinedAssociationsUids = data?.get("joinedAssociations") as? List<String> ?: emptyList()
-  val joinedAssociations = Association.firestoreReferenceListWith(joinedAssociationsUids)
+    val joinedAssociationsUids = data?.get("joinedAssociations") as? List<String> ?: emptyList()
+    val joinedAssociations = Association.firestoreReferenceListWith(joinedAssociationsUids)
 
-  return User(
-      uid = data?.get("uid") as? String ?: "",
-      email = data?.get("email") as? String ?: "",
-      firstName = data?.get("firstName") as? String ?: "",
-      lastName = data?.get("lastName") as? String ?: "",
-      biography = data?.get("biography") as? String ?: "",
-      followedAssociations = followingAssociations,
-      joinedAssociations = joinedAssociations,
-      interests =
-          (data?.get("interests") as? List<String> ?: emptyList()).map { Interest.valueOf(it) },
-      socials =
-          (data?.get("socials") as? List<Map<String, String>> ?: emptyList()).map {
+    return User(
+        uid = data?.get("uid") as? String ?: "",
+        email = data?.get("email") as? String ?: "",
+        firstName = data?.get("firstName") as? String ?: "",
+        lastName = data?.get("lastName") as? String ?: "",
+        biography = data?.get("biography") as? String ?: "",
+        followedAssociations = followingAssociations,
+        joinedAssociations = joinedAssociations,
+        interests =
+        (data?.get("interests") as? List<String> ?: emptyList()).map { Interest.valueOf(it) },
+        socials =
+        (data?.get("socials") as? List<Map<String, String>> ?: emptyList()).map {
             UserSocial(Social.valueOf(it["social"] ?: ""), it["content"] ?: "")
-          },
-      profilePicture = data?.get("profilePicture") as? String ?: "",
-      hasProvidedAccountDetails = data?.get("hasProvidedAccountDetails") as? Boolean ?: false)
+        },
+        profilePicture = data?.get("profilePicture") as? String ?: "",
+        hasProvidedAccountDetails = data?.get("hasProvidedAccountDetails") as? Boolean ?: false
+    )
 }
 
 fun EventRepositoryFirestore.Companion.hydrate(data: Map<String, Any>?): Event {
-  val organisers =
-      Association.firestoreReferenceListWith(
-          data?.get("organisers") as? List<String> ?: emptyList())
+    val organisers =
+        Association.firestoreReferenceListWith(
+            data?.get("organisers") as? List<String> ?: emptyList()
+        )
 
-  val taggedAssociations =
-      Association.firestoreReferenceListWith(
-          data?.get("taggedAssociations") as? List<String> ?: emptyList())
+    val taggedAssociations =
+        Association.firestoreReferenceListWith(
+            data?.get("taggedAssociations") as? List<String> ?: emptyList()
+        )
 
-  val types = (data?.get("types") as? List<String> ?: emptyList())
+    val types = (data?.get("types") as? List<String> ?: emptyList())
 
-  val location = data?.get("location") as? Map<String, Any> ?: emptyMap()
+    val location = data?.get("location") as? Map<String, Any> ?: emptyMap()
 
-  return Event(
-      uid = data?.get("uid") as? String ?: "",
-      title = data?.get("title") as? String ?: "",
-      organisers = organisers,
-      taggedAssociations = taggedAssociations,
-      image = data?.get("image") as? String ?: "",
-      description = data?.get("description") as? String ?: "",
-      catchyDescription = data?.get("catchyDescription") as? String ?: "",
-      price = data?.get("price") as? Double ?: 0.0,
-      date = data?.get("date") as? Timestamp ?: Timestamp(0, 0),
-      location =
-          Location(
-              latitude = location.get("latitude") as? Double ?: 0.0,
-              longitude = location.get("longitude") as? Double ?: 0.0,
-              name = location.get("name") as? String ?: ""),
-      types = types.map { EventType.valueOf(it) })
+    return Event(
+        uid = data?.get("uid") as? String ?: "",
+        title = data?.get("title") as? String ?: "",
+        organisers = organisers,
+        taggedAssociations = taggedAssociations,
+        image = data?.get("image") as? String ?: "",
+        description = data?.get("description") as? String ?: "",
+        catchyDescription = data?.get("catchyDescription") as? String ?: "",
+        price = data?.get("price") as? Double ?: 0.0,
+        date = data?.get("date") as? Timestamp ?: Timestamp(0, 0),
+        location =
+        Location(
+            latitude = location.get("latitude") as? Double ?: 0.0,
+            longitude = location.get("longitude") as? Double ?: 0.0,
+            name = location.get("name") as? String ?: ""
+        ),
+        types = types.map { EventType.valueOf(it) })
 }
