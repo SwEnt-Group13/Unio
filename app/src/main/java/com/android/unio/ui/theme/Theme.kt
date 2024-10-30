8a2cd4854f2e234e35df2e77593b230df57c7b8e
Oskar Codes <oskarbertrand@icloud.com>
2024-10-30 19:09:16 +0100

    feat(AppTheme): Update AppTheme to use the preference provided by the PreferenceManager

--- app/src/main/java/com/android/unio/ui/theme/Theme.kt
+++ app/src/main/java/com/android/unio/ui/theme/Theme.kt
@@ -11,11 +11,13 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+import androidx.preference.PreferenceManager
 
 private val lightScheme =
     lightColorScheme(
@@ -262,28 +264,40 @@ data class ColorFamily(
 val unspecified_scheme =
     ColorFamily(Color.Unspecified, Color.Unspecified, Color.Unspecified, Color.Unspecified)
 
+object Theme {
+  const val LIGHT = "Light"
+  const val DARK = "Dark"
+  const val SYSTEM = "System Default"
+}
+
 @Composable
 fun AppTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = false,
-    content: @Composable() () -> Unit
+    content: @Composable () -> Unit
 ) {
+  val theme =
+      PreferenceManager.getDefaultSharedPreferences(LocalContext.current)
+          .getString("theme", Theme.SYSTEM) ?: Theme.SYSTEM
+
   val colorScheme =
       when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
           val context = LocalContext.current
-          if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+          if (theme == Theme.DARK) dynamicDarkColorScheme(context)
+          else dynamicLightColorScheme(context)
         }
-        darkTheme -> darkScheme
-        else -> lightScheme
+        theme == Theme.DARK -> darkScheme
+        theme == Theme.LIGHT -> lightScheme
+        else -> if (isSystemInDarkTheme()) darkScheme else lightScheme
       }
   val view = LocalView.current
   if (!view.isInEditMode) {
     SideEffect {
       val window = (view.context as Activity).window
       window.statusBarColor = colorScheme.primary.toArgb()
-      WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+      WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars =
+          theme == Theme.DARK
     }
   }
 
