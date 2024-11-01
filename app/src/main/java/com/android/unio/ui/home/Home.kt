142ac7e831257aa3b4194d90c0251d84a519658d
Oskar Codes <oskarbertrand@icloud.com>
2024-11-01 17:07:10 +0100

    chore(Home): remove unused import

--- app/src/main/java/com/android/unio/ui/home/Home.kt
+++ app/src/main/java/com/android/unio/ui/home/Home.kt
@@ -41,7 +41,6 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.android.unio.model.event.EventCard
 import com.android.unio.model.event.EventListViewModel
 import com.android.unio.model.user.UserViewModel
 import com.android.unio.ui.event.EventCard
