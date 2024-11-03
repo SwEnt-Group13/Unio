114d4d6831328d6dc28fa1b043a10179d5ffea3e
Aurelien9Code <152640312+Aurelien9Code@users.noreply.github.com>
2024-11-03 20:47:21 +0100

    feat(mock-test): update mock ReferenceList with new definition

--- app/src/main/java/com/android/unio/mocks/firestore/MockReferenceList.kt
+++ app/src/main/java/com/android/unio/mocks/firestore/MockReferenceList.kt
@@ -7,12 +7,19 @@ import kotlinx.coroutines.flow.StateFlow
 class MockReferenceList<T>(elements: List<T> = emptyList()) : ReferenceList<T> {
   private val _list = MutableStateFlow(elements)
   override val list: StateFlow<List<T>> = _list
+  override val uids: List<String> = emptyList()
 
   override fun add(uid: String) {}
 
   override fun addAll(uids: List<String>) {}
 
+  override fun remove(uid: String) {}
+
   override fun requestAll(onSuccess: () -> Unit) {
     onSuccess()
+  }
+
+  override fun contains(uid: String): Boolean {
+    return false
   }
 }
