2ef23e497c4cfa726e387927fd3a330919a46a56
Romain Hirschi <romain.hirschi@epfl.ch>
2024-11-06 10:58:04 +0100

    test: update AssociationProfileTest using DI properly

--- app/src/androidTest/java/com/android/unio/ui/association/AssociationProfileTest.kt
+++ app/src/androidTest/java/com/android/unio/ui/association/AssociationProfileTest.kt
@@ -17,37 +17,47 @@ import com.android.unio.model.association.AssociationViewModel
 import com.android.unio.model.event.Event
 import com.android.unio.model.event.EventRepository
 import com.android.unio.model.firestore.firestoreReferenceListWith
+import com.android.unio.model.hilt.module.NavHostControllerModule
 import com.android.unio.model.user.User
 import com.android.unio.ui.navigation.NavigationAction
-import com.google.firebase.firestore.CollectionReference
-import com.google.firebase.firestore.FirebaseFirestore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Inject
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mock
+import org.mockito.Mockito.mock
 import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
 import org.mockito.kotlin.any
-import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 
+@HiltAndroidTest
 class AssociationProfileTest {
-  private lateinit var navHostController: NavHostController
-  private lateinit var navigationAction: NavigationAction
-  @Mock private lateinit var collectionReference: CollectionReference
-  @Mock private lateinit var db: FirebaseFirestore
-  @Mock private lateinit var associationRepository: AssociationRepository
-  @Mock private lateinit var eventRepository: EventRepository
+
+  @Inject lateinit var navigationAction: NavigationAction
+
+  @Mock lateinit var associationRepository: AssociationRepository
+  @Mock lateinit var eventRepository: EventRepository
+
   private lateinit var associationViewModel: AssociationViewModel
 
   private lateinit var associations: List<Association>
   private lateinit var events: List<Event>
 
   @get:Rule val composeTestRule = createComposeRule()
+  @get:Rule val hiltRule = HiltAndroidRule(this)
 
   @Before
   fun setUp() {
     MockitoAnnotations.openMocks(this)
+    hiltRule.inject()
 
     associations =
         listOf(
@@ -97,7 +107,6 @@ class AssociationProfileTest {
                 price = 0.0,
             ))
 
-    `when`(db.collection(any())).thenReturn(collectionReference)
     `when`(associationRepository.getAssociations(any(), any())).thenAnswer { invocation ->
       val onSuccess = invocation.arguments[0] as (List<Association>) -> Unit
       onSuccess(associations)
@@ -106,9 +115,6 @@ class AssociationProfileTest {
       val onSuccess = invocation.arguments[1] as (List<Event>) -> Unit
       onSuccess(events)
     }
-
-    navHostController = mock { NavHostController::class.java }
-    navigationAction = NavigationAction(navHostController)
 
     associationViewModel = AssociationViewModel(associationRepository, eventRepository)
     associationViewModel.getAssociations()
@@ -188,9 +194,15 @@ class AssociationProfileTest {
       AssociationProfileScreen(navigationAction, "", associationViewModel)
     }
 
+    val field = NavigationAction::class.java.getDeclaredField("navController")
+    field.isAccessible = true
+    val navController = field.get(navigationAction) as NavHostController
+
+    `when`(navController.popBackStack()).thenReturn(true)
+
     composeTestRule.onNodeWithTag("goBackButton").performClick()
 
-    verify(navHostController).popBackStack()
+    verify(navController).popBackStack()
   }
 
   @Test
