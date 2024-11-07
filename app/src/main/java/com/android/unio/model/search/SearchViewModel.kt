a96467c9bb83d353b74a12b26aa1d692300617c4
Redd87 <eddy.p.rashed@gmail.com>
2024-11-07 21:28:10 +0100

    chore: remove unused import

--- app/src/main/java/com/android/unio/model/search/SearchViewModel.kt
+++ app/src/main/java/com/android/unio/model/search/SearchViewModel.kt
@@ -1,7 +1,6 @@
 package com.android.unio.model.search
 
 import android.content.Context
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
