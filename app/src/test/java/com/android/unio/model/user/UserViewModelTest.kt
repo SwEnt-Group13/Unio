8b2d96f0d5c5700e0097b39b0c8de69c0cf2c50f
Arnaud Rajon <arnaud.rajon@epfl.ch>
2024-11-07 18:25:57 +0100

    test(model/user): add a test for updateUser

--- app/src/test/java/com/android/unio/model/user/UserViewModelTest.kt
+++ app/src/test/java/com/android/unio/model/user/UserViewModelTest.kt
@@ -42,4 +42,20 @@ class UserViewModelTest {
     // Check that user is set to null
     assertEquals(user, userViewModel.user.value)
   }
+
+  @Test
+  fun testUpdateUser() {
+    val user = MockUser.createMockUser(uid="1")
+    every {repository.updateUser(any(), any(), any())} answers {
+      val onSuccess = it.invocation.args[1] as () -> Unit
+      onSuccess()
+    }
+    every {repository.getUserWithId("1", any(), any())} answers {
+      val onSuccess = it.invocation.args[1] as (User) -> Unit
+      onSuccess(user)
+    }
+    userViewModel.updateUser(user)
+
+    verify { repository.updateUser(user, any(), any()) }
+  }
 }
