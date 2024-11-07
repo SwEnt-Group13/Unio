196bf7cb1a150f20adfc7ed220b78521fc3e32d1
Zafar <isadjanovzafar@gmail.com>
2024-11-07 20:42:36 +0100

    chore(test): adapt MapScreenTest to updates from main

--- app/src/androidTest/java/com/android/unio/ui/map/MapScreenTest.kt
+++ app/src/androidTest/java/com/android/unio/ui/map/MapScreenTest.kt
@@ -5,41 +5,66 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
+import androidx.navigation.NavHostController
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.android.unio.model.event.EventRepository
+import com.android.unio.mocks.user.MockUser
+import com.android.unio.model.event.EventRepositoryFirestore
 import com.android.unio.model.event.EventViewModel
+import com.android.unio.model.user.User
+import com.android.unio.model.user.UserRepositoryFirestore
+import com.android.unio.model.user.UserViewModel
 import com.android.unio.model.image.ImageRepository
+import com.android.unio.model.image.ImageRepositoryFirebaseStorage
 import com.android.unio.ui.navigation.NavigationAction
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.verify
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mock
-import org.mockito.Mockito.mock
-import org.mockito.MockitoAnnotations
-import org.mockito.kotlin.verify
 
 @RunWith(AndroidJUnit4::class)
 class MapScreenTest {
+  private val user = MockUser.createMockUser()
 
   @get:Rule val composeTestRule = createComposeRule()
 
-  @Mock private lateinit var eventRepository: EventRepository
-  @Mock private lateinit var imageRepository: ImageRepository
+  @MockK private lateinit var eventRepository: EventRepositoryFirestore
+  @MockK private lateinit var userRepository: UserRepositoryFirestore
+  @MockK private lateinit var navHostController: NavHostController
+  @MockK private lateinit var imageRepository: ImageRepositoryFirebaseStorage
   private lateinit var navigationAction: NavigationAction
   private lateinit var eventViewModel: EventViewModel
+  private lateinit var userViewModel: UserViewModel
 
   @Before
   fun setUp() {
-    MockitoAnnotations.openMocks(this)
+    MockKAnnotations.init(this, relaxed = true)
 
-    navigationAction = mock(NavigationAction::class.java)
+    navigationAction = NavigationAction(navHostController)
+
+    every { eventRepository.init(any()) } answers {}
     eventViewModel = EventViewModel(eventRepository, imageRepository)
+
+    every { userRepository.init(any()) } returns Unit
+    every { userRepository.getUserWithId("123", any(), any()) } answers
+        {
+          val onSuccess = it.invocation.args[1] as (User) -> Unit
+          onSuccess(user)
+        }
+    userViewModel = UserViewModel(userRepository, false)
+    userViewModel.getUserByUid("123")
   }
 
   @Test
   fun mapScreenComponentsAreDisplayed() {
-    composeTestRule.setContent { MapScreen(navigationAction, eventViewModel) }
+    composeTestRule.setContent {
+      MapScreen(
+          navigationAction = navigationAction,
+          eventViewModel = eventViewModel)
+    }
 
     composeTestRule.onNodeWithTag("MapScreen").assertIsDisplayed()
     composeTestRule.onNodeWithTag("MapTitle").assertIsDisplayed()
@@ -48,9 +73,13 @@ class MapScreenTest {
 
   @Test
   fun mapScreenBackButtonNavigatesBack() {
-    composeTestRule.setContent { MapScreen(navigationAction, eventViewModel) }
+    composeTestRule.setContent {
+      MapScreen(
+          navigationAction = navigationAction,
+          eventViewModel = eventViewModel)
+    }
 
     composeTestRule.onNodeWithTag("goBackButton").performClick()
-    verify(navigationAction).goBack()
+    verify { navigationAction.goBack() }
   }
 }
