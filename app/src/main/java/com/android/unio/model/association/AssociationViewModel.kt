1f8c00c9cb55cc4a07d88dd8a8022c7caded9b01
Zafar <isadjanovzafar@gmail.com>
2024-11-07 15:40:26 +0100

    chore(association-viewmodel): remove unnecessary factory object and coroutines
    - Removed companion object for Factory instantiation.
    - Removed coroutine launch.

--- app/src/main/java/com/android/unio/model/association/AssociationViewModel.kt
+++ app/src/main/java/com/android/unio/model/association/AssociationViewModel.kt
@@ -2,18 +2,12 @@ package com.android.unio.model.association
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
 import com.android.unio.model.event.Event
 import com.android.unio.model.event.EventRepository
-import com.android.unio.model.event.EventRepositoryFirestore
 import com.android.unio.model.image.ImageRepositoryFirebaseStorage
-import com.google.firebase.Firebase
-import com.google.firebase.firestore.firestore
 import java.io.InputStream
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
 
 class AssociationViewModel(
     private val associationRepository: AssociationRepository,
@@ -32,31 +26,16 @@ class AssociationViewModel(
     associationRepository.init { getAssociations() }
   }
 
-  companion object {
-    val Factory: ViewModelProvider.Factory =
-        object : ViewModelProvider.Factory {
-          @Suppress("UNCHECKED_CAST")
-          override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            return AssociationViewModel(
-                AssociationRepositoryFirestore(Firebase.firestore),
-                EventRepositoryFirestore(Firebase.firestore))
-                as T
-          }
-        }
-  }
-
   fun getEventsForAssociation(association: Association, onSuccess: (List<Event>) -> Unit) {
-    viewModelScope.launch {
-      eventRepository.getEventsOfAssociation(
-          association.uid,
-          onSuccess = onSuccess,
-          onFailure = { exception ->
-            Log.e(
-                "ExploreViewModel",
-                "Failed to get events for association ${association.fullName}",
-                exception)
-          })
-    }
+    eventRepository.getEventsOfAssociation(
+        association.uid,
+        onSuccess = onSuccess,
+        onFailure = { exception ->
+          Log.e(
+              "ExploreViewModel",
+              "Failed to get events for association ${association.fullName}",
+              exception)
+        })
   }
 
   /**
@@ -65,17 +44,15 @@ class AssociationViewModel(
    * set to an empty list.
    */
   fun getAssociations() {
-    viewModelScope.launch {
-      associationRepository.getAssociations(
-          onSuccess = { fetchedAssociations ->
-            _associations.value = fetchedAssociations
-            _associationsByCategory.value = fetchedAssociations.groupBy { it.category }
-          },
-          onFailure = { exception ->
-            _associations.value = emptyList()
-            Log.e("ExploreViewModel", "Failed to fetch associations", exception)
-          })
-    }
+    associationRepository.getAssociations(
+        onSuccess = { fetchedAssociations ->
+          _associations.value = fetchedAssociations
+          _associationsByCategory.value = fetchedAssociations.groupBy { it.category }
+        },
+        onFailure = { exception ->
+          _associations.value = emptyList()
+          Log.e("ExploreViewModel", "Failed to fetch associations", exception)
+        })
   }
 
   fun addAssociation(
@@ -84,16 +61,14 @@ class AssociationViewModel(
       onSuccess: () -> Unit,
       onFailure: (Exception) -> Unit
   ) {
-    viewModelScope.launch {
-      imageRepository.uploadImage(
-          inputStream,
-          "images/associations/${association.uid}",
-          { uri ->
-            association.image = uri
-            associationRepository.addAssociation(association, onSuccess, onFailure)
-          },
-          { e -> Log.e("ImageRepository", "Failed to store image : $e") })
-    }
+    imageRepository.uploadImage(
+        inputStream,
+        "images/associations/${association.uid}",
+        { uri ->
+          association.image = uri
+          associationRepository.addAssociation(association, onSuccess, onFailure)
+        },
+        { e -> Log.e("ImageRepository", "Failed to store image : $e") })
   }
 
   /**
