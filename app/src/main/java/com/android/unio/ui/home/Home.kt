b42365dd7e4dbb188ef057fe7dcc1bdf3927b276
Arnaud Rajon <arnaud.rajon@epfl.ch>
2024-11-14 10:35:59 +0100

    feature(Home): Refactor top bar
    Refactored top bar so the little bar indicator moves along with the scroll, making a cool effect. Took me a lot of time

--- app/src/main/java/com/android/unio/ui/home/Home.kt
+++ app/src/main/java/com/android/unio/ui/home/Home.kt
@@ -1,6 +1,5 @@
 package com.android.unio.ui.home
 
-import androidx.compose.animation.core.Animatable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -19,26 +18,35 @@ import androidx.compose.material3.DockedSearchBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.android.unio.R
 import com.android.unio.model.event.Event
 import com.android.unio.model.event.EventViewModel
@@ -51,8 +59,8 @@ import com.android.unio.ui.navigation.LIST_TOP_LEVEL_DESTINATION
 import com.android.unio.ui.navigation.NavigationAction
 import com.android.unio.ui.navigation.Route
 import com.android.unio.ui.navigation.Screen
+import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     navigationAction: NavigationAction,
@@ -60,24 +68,11 @@ fun HomeScreen(
     userViewModel: UserViewModel,
     searchViewModel: SearchViewModel
 ) {
-
-    val density = LocalDensity.current.density
-
-    val coroutineScope = rememberCoroutineScope()
-    val animatablePosition = remember { Animatable(0f) }
-
-    var allTabWidth by remember { mutableStateOf(0.dp) }
-    var followingTabWidth by remember { mutableStateOf(0.dp) }
-    var allTabXCoordinate by remember { mutableStateOf(0f) }
-    var followingTabXCoordinate by remember { mutableStateOf(0f) }
-
-    val horizontalHeaderPadding = 16.dp
     val context = LocalContext.current
-
-    var searchQuery by remember { mutableStateOf("") }
 
     val searchResults by searchViewModel.events.collectAsState()
     val searchState by searchViewModel.status.collectAsState()
+
 
     Scaffold(
         floatingActionButton = {
@@ -99,186 +94,29 @@ fun HomeScreen(
         },
         modifier = Modifier.testTag(HomeTestTags.SCREEN),
         content = { paddingValues ->
-            Column() {
 
-                val tabItems = listOf(
-                    "All", "Following"
-                )
-                var selectedTabIndex by remember {
-                    mutableIntStateOf(0)
-                }
-                val pagerState = rememberPagerState {
-                    tabItems.size
-                }
-                LaunchedEffect(key1 = selectedTabIndex) {
-                    pagerState.animateScrollToPage(selectedTabIndex)
-                }
-
-                LaunchedEffect(key1 = pagerState.currentPage, pagerState.isScrollInProgress) {
-                    if (!pagerState.isScrollInProgress)
-                        selectedTabIndex = pagerState.currentPage
-                }
-                Column(modifier = Modifier.fillMaxWidth()) {
-
-                    TabRow(selectedTabIndex = selectedTabIndex) {
-                        tabItems.forEachIndexed { index, tabItem ->
-
-                            Tab(
-                                selected = index == selectedTabIndex,
-                                onClick = {
-                                    selectedTabIndex = index
-                                },
-                                text = { Text(text = tabItem) },
-
-                                )
-                        }
-                    }
-                    DockedSearchBar(
-                        inputField = {
-                            SearchBarDefaults.InputField(
-                                modifier = Modifier.testTag(HomeTestTags.SEARCH_BAR_INPUT),
-                                query = searchQuery,
-                                onQueryChange = {
-                                    searchQuery = it
-                                    searchViewModel.debouncedSearch(
-                                        it,
-                                        SearchViewModel.SearchType.EVENT
-                                    )
-                                },
-                                onSearch = {},
-                                expanded = false,
-                                onExpandedChange = {},
-                                placeholder = {
-                                    Text(text = context.getString(R.string.explore_search_placeholder))
-                                },
-                                trailingIcon = {
-                                    if (searchState == SearchViewModel.Status.LOADING) {
-                                        CircularProgressIndicator()
-                                    } else {
-                                        Icon(
-                                            Icons.Default.Search,
-                                            contentDescription =
-                                            context.getString(R.string.home_content_description_search_icon)
-                                        )
-                                    }
-                                },
-                            )
-                        },
-                        expanded = false,
-                        onExpandedChange = {},
-                        modifier = Modifier
-                            .padding(horizontal = 16.dp)
-                            .testTag(HomeTestTags.SEARCH_BAR)
-                    ) {}
-
-                    HorizontalPager(
-                        state = pagerState, modifier = Modifier
-                            .fillMaxWidth()
-                    ) { index ->
-                        HomeContent(navigationAction, index, searchQuery, searchState, searchResults, userViewModel, eventViewModel, paddingValues)
-                    }
-                }
-
-            /*Column(modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)) {
-                // Sticky Header
-                Box(
-                    modifier =
-                    Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 16.dp, horizontal = horizontalHeaderPadding)
-                        .testTag(HomeTestTags.HEADER)
-                ) {
-                    Column {
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.SpaceBetween
-                        ) {
-                            Text(
-                                text = "All",
-                                color =
-                                if (selectedTab == "All") MaterialTheme.colorScheme.primary
-                                else MaterialTheme.colorScheme.inversePrimary,
-                                modifier =
-                                Modifier
-                                    .clickable {
-                                        selectedTab = "All"
-                                        coroutineScope.launch {
-                                            animatablePosition.animateTo(
-                                                0f, // Starting point for "All"
-                                                animationSpec =
-                                                tween(durationMillis = 1000) // Animation duration
-                                            )
-                                        }
-                                    }
-                                    .padding(horizontal = 16.dp)
-                                    .onGloballyPositioned { coordinates ->
-                                        allTabWidth = (coordinates.size.width / density).dp
-                                        allTabXCoordinate = coordinates.positionInRoot().x
-                                    }
-                                    .testTag(HomeTestTags.TAB_ALL))
-
-                            // Clickable text for "Following"
-                            Text(
-                                text = "Following",
-                                color =
-                                if (selectedTab == "Following") MaterialTheme.colorScheme.primary
-                                else MaterialTheme.colorScheme.inversePrimary,
-                                modifier =
-                                Modifier
-                                    .clickable {
-                                        selectedTab = "Following"
-                                        coroutineScope.launch {
-                                            animatablePosition.animateTo(
-                                                1f, // Ending point for "Following"
-                                                animationSpec =
-                                                tween(durationMillis = 1000) // Animation duration
-                                            )
-                                        }
-                                    }
-                                    .padding(horizontal = 16.dp)
-                                    .onGloballyPositioned { coordinates ->
-                                        followingTabWidth = (coordinates.size.width / density).dp
-                                        followingTabXCoordinate = coordinates.positionInRoot().x
-                                    }
-                                    .testTag(HomeTestTags.TAB_SAVED))
-                        }
-
-                        // Underline to indicate selected tab with smooth sliding animation
-                        Box(modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 4.dp)) {
-                            val selectedTabWidth =
-                                if (selectedTab == "All") allTabWidth else followingTabWidth
-                            Box(
-                                modifier =
-                                Modifier
-                                    .offset(
-                                        x =
-                                        ((animatablePosition.value *
-                                                (followingTabXCoordinate - allTabXCoordinate) +
-                                                allTabXCoordinate) / density)
-                                            .dp - horizontalHeaderPadding
-                                    )
-                                    .width(selectedTabWidth)
-                                    .height(2.dp)
-                                    .background(Color.Blue)
-                                    .testTag(HomeTestTags.UNDERLYING_BAR)
-                            )
-                        }
-                    }
-                }*/
-
-
-
-
-            }})
-        }
+            TopBar(
+                navigationAction,
+                searchState,
+                searchResults,
+                userViewModel,
+                searchViewModel,
+                eventViewModel,
+                paddingValues
+            )
+        })
+}
 
 @Composable
-fun HomeContent(navigationAction:NavigationAction, topBarIndex: Int, searchQuery: String, searchState: SearchViewModel.Status, searchResults: List<Event>,
-                userViewModel: UserViewModel, eventViewModel: EventViewModel, paddingValues: PaddingValues) {
+fun HomeContent(
+    navigationAction: NavigationAction,
+    searchQuery: String,
+    searchState: SearchViewModel.Status,
+    searchResults: List<Event>,
+    userViewModel: UserViewModel,
+    eventViewModel: EventViewModel,
+    paddingValues: PaddingValues
+) {
 
     val context = LocalContext.current
     val events by eventViewModel.events.collectAsState()
@@ -317,8 +155,14 @@ fun HomeContent(navigationAction:NavigationAction, topBarIndex: Int, searchQuery
     }
 
 }
+
 @Composable
-fun EventList(navigationAction: NavigationAction,events: List<Event>, userViewModel: UserViewModel, eventViewModel: EventViewModel) {
+fun EventList(
+    navigationAction: NavigationAction,
+    events: List<Event>,
+    userViewModel: UserViewModel,
+    eventViewModel: EventViewModel
+) {
     LazyColumn(
         contentPadding = PaddingValues(vertical = 8.dp),
         modifier = Modifier
@@ -327,6 +171,157 @@ fun EventList(navigationAction: NavigationAction,events: List<Event>, userViewMo
     ) {
         items(events) { event ->
             EventCard(navigationAction, event, userViewModel, eventViewModel)
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(
+    navigationAction: NavigationAction,
+    searchState: SearchViewModel.Status,
+    searchResults: List<Event>,
+    userViewModel: UserViewModel,
+    searchViewModel: SearchViewModel,
+    eventViewModel: EventViewModel,
+    paddingValues: PaddingValues
+) {
+
+
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .padding(paddingValues)
+            .padding(top = 20.dp, bottom = 50.dp)
+            .fillMaxSize()
+    ) {
+        var searchQuery by remember { mutableStateOf("") }
+
+        val list = listOf("All", "Following")
+        val scope = rememberCoroutineScope()
+        val pagerState = rememberPagerState(initialPage = 0) { 2 }
+
+        val sizeList = remember { mutableStateMapOf<Int, Pair<Float, Float>>() }
+
+        val progressFromFirstPage by remember {
+            derivedStateOf {
+                pagerState.currentPageOffsetFraction + pagerState.currentPage.dp.value
+            }
+        }
+        val colorScheme = MaterialTheme.colorScheme
+        TabRow(
+            selectedTabIndex = pagerState.currentPage,
+            contentColor = Color(0xFF362C28),
+            divider = {},
+            indicator = {
+                Box(modifier = Modifier
+                    .fillMaxSize()
+                    .drawBehind {
+
+                        val tabWidth = sizeList[0]!!.first
+                        val height = sizeList[0]!!.second
+                        val startOffset = Offset(
+                            x = progressFromFirstPage * tabWidth + tabWidth / 3,
+                            y = height - 25
+                        )
+                        val endOffset = Offset(
+                            x = progressFromFirstPage * tabWidth + tabWidth * 2 / 3,
+                            y = height - 25
+                        )
+
+                        drawLine(
+                            start = startOffset,
+                            end = endOffset,
+                            color = colorScheme.primary,
+                            strokeWidth = Stroke.DefaultMiter
+                        )
+
+                    }
+                )
+            }
+        ) {
+            list.forEachIndexed { index, str ->
+                Tab(
+                    selected = index == pagerState.currentPage,
+                    onClick = {
+                        scope.launch {
+                            pagerState.animateScrollToPage(index)
+                        }
+                    },
+                    modifier = Modifier
+                        .onSizeChanged {
+                            sizeList[index] = Pair(it.width.toFloat(), it.height.toFloat())
+                        },
+                    selectedContentColor = colorScheme.primary
+                ) {
+                    Text(
+                        text = str,
+                        style = TextStyle(
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 12.sp,
+                        ),
+                        modifier = Modifier
+                            .align(CenterHorizontally)
+                            .padding(horizontal = 32.dp, vertical = 16.dp)
+                    )
+                }
+            }
+        }
+
+        DockedSearchBar(
+            inputField = {
+                SearchBarDefaults.InputField(
+                    modifier = Modifier.testTag(HomeTestTags.SEARCH_BAR_INPUT),
+                    query = searchQuery,
+                    onQueryChange = {
+                        searchQuery = it
+                        searchViewModel.debouncedSearch(
+                            it,
+                            SearchViewModel.SearchType.EVENT
+                        )
+                    },
+                    onSearch = {},
+                    expanded = false,
+                    onExpandedChange = {},
+                    placeholder = {
+                        Text(text = context.getString(R.string.explore_search_placeholder))
+                    },
+                    trailingIcon = {
+                        if (searchState == SearchViewModel.Status.LOADING) {
+                            CircularProgressIndicator()
+                        } else {
+                            Icon(
+                                Icons.Default.Search,
+                                contentDescription =
+                                context.getString(R.string.home_content_description_search_icon)
+                            )
+                        }
+                    },
+                )
+            },
+            expanded = false,
+            onExpandedChange = {},
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .testTag(HomeTestTags.SEARCH_BAR)
+        ) {}
+
+
+        HorizontalPager(
+            state = pagerState, modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            HomeContent(
+                navigationAction,
+                searchQuery,
+                searchState,
+                searchResults,
+                userViewModel,
+                eventViewModel,
+                paddingValues
+            )
         }
     }
 }
