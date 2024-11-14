dd2bd3f188b2438f46586a612fc7ea6e983a8148
Alexei Thornber <alexeithornber@gmail.com>
2024-11-14 22:58:41 +0100

    test(SearchTest): Add functioning search test

--- app/src/androidTest/java/com/android/unio/ui/end2end/SearchTest.kt
+++ app/src/androidTest/java/com/android/unio/ui/end2end/SearchTest.kt
@@ -1,13 +1,19 @@
 package com.android.unio.ui.end2end
 
+import androidx.compose.ui.test.assertCountEquals
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.isDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performScrollTo
 import androidx.compose.ui.test.performTextInput
 import androidx.test.filters.LargeTest
 import com.android.unio.MainActivity
+import com.android.unio.model.strings.test_tags.EventCardTestTags
+import com.android.unio.model.strings.test_tags.EventDetailsTestTags
 import com.android.unio.model.strings.test_tags.HomeTestTags
 import com.android.unio.model.strings.test_tags.WelcomeTestTags
 import com.android.unio.ui.end2end.UserAccountCreationTest.Companion.EMAIL
@@ -15,6 +21,7 @@ import com.android.unio.ui.end2end.UserAccountCreationTest.Companion.PWD
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Rule
+import org.junit.Test
 
 
 @LargeTest
@@ -24,26 +31,31 @@ class SearchTest : EndToEndTest() {
     @get:Rule val composeTestRule = createAndroidComposeRule<MainActivity>()
     @get:Rule val hiltRule = HiltAndroidRule(this)
 
-
-    fun testSearchDisplaysCorrectResults(){
+    @Test
+    fun testSearchDisplaysCorrectResultsForEvents(){
         composeTestRule.onNodeWithTag(WelcomeTestTags.SCREEN).assertIsDisplayed()
         composeTestRule.onNodeWithTag(WelcomeTestTags.EMAIL).performTextInput(User1.EMAIL)
         composeTestRule.onNodeWithTag(WelcomeTestTags.PASSWORD).performTextInput(User1.PASSWORD)
+        composeTestRule.onNodeWithTag(WelcomeTestTags.BUTTON).performClick()
 
         composeTestRule.waitUntil (5000) {
             composeTestRule.onNodeWithTag(HomeTestTags.SCREEN).isDisplayed()
         }
 
+        Thread.sleep(20000)
         composeTestRule.onNodeWithTag(HomeTestTags.SEARCH_BAR_INPUT).assertIsDisplayed()
-        composeTestRule.onNodeWithTag(HomeTestTags.SEARCH_BAR_INPUT).performTextInput("Weekendski")
+        composeTestRule.onNodeWithTag(HomeTestTags.SEARCH_BAR_INPUT).performTextInput("Weekend")
 
         //Wait for "server's" response to get the event
+        Thread.sleep(5000)
+        composeTestRule.onAllNodesWithTag(EventCardTestTags.EVENT_ITEM).assertCountEquals(1)
 
-//        composeTestRule.waitUntil (5000) {
-//        }
+        composeTestRule.onNodeWithTag(EventCardTestTags.EVENT_ITEM).performClick()
 
+        composeTestRule.waitUntil (5000) {
+            composeTestRule.onNodeWithTag(EventDetailsTestTags.SCREEN).isDisplayed()
+        }
 
-
-
+        composeTestRule.onNodeWithTag(EventDetailsTestTags.TITLE).assertTextEquals("WeekEndSki IC")
     }
 }
