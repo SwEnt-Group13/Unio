433931aae712ca22adb42a452c50f26bf8f72209
Zafar <isadjanovzafar@gmail.com>
2024-11-18 11:44:37 +0100

    feat(map-viewmodel): add functions to handle periodic GPS refreshes

--- app/src/main/java/com/android/unio/model/map/MapViewModel.kt
+++ app/src/main/java/com/android/unio/model/map/MapViewModel.kt
@@ -2,10 +2,15 @@ package com.android.unio.model.map
 
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Looper
 import android.util.Log
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModel
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.Priority
 import com.google.android.gms.maps.model.LatLng
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -22,6 +27,8 @@ constructor(private val fusedLocationClient: FusedLocationProviderClient) : View
   private val _userLocation = MutableStateFlow<LatLng?>(null)
   val userLocation: StateFlow<LatLng?> = _userLocation.asStateFlow()
 
+  private var locationCallback: LocationCallback? = null
+
   /** Fetches the user's location and updates the [_userLocation] state flow. */
   fun fetchUserLocation(context: Context) {
     if (hasLocationPermissions(context)) {
@@ -34,6 +41,37 @@ constructor(private val fusedLocationClient: FusedLocationProviderClient) : View
       }
     } else {
       Log.e("MapViewModel", "Location permission is not granted.")
+    }
+  }
+
+  fun startLocationUpdates(context: Context) {
+    if (hasLocationPermissions(context)) {
+      val locationRequest = LocationRequest.Builder(10000)
+        .setMinUpdateIntervalMillis(5000)
+        .setPriority(Priority.PRIORITY_BALANCED_POWER_ACCURACY)
+        .build()
+
+      locationCallback = object: LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+          locationResult.lastLocation?.let {
+            _userLocation.value = LatLng(it.latitude, it.longitude)
+          }
+        }
+      }
+
+      try {
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback!!, Looper.getMainLooper())
+      } catch (e: SecurityException) {
+        Log.e("MapViewModel", "Permission for location access was revoked: ${e.localizedMessage}")
+      }
+    } else {
+        Log.e("MapViewModel", "Location permission is not granted.")
+    }
+  }
+
+  fun stopLocationUpdates() {
+    locationCallback?.let {
+      fusedLocationClient.removeLocationUpdates(it)
     }
   }
 
