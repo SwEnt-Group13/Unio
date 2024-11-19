e3033d9473f484a666bd3f965767ef87a7af54f6
Aurelien9Code <152640312+Aurelien9Code@users.noreply.github.com>
2024-11-20 00:17:45 +0100

    test(permissions-&-access-rights): add e2e test for claiming association rights

--- app/src/androidTest/java/com/android/unio/end2end/ClaimAdminRightsTest.kt
+++ app/src/androidTest/java/com/android/unio/end2end/ClaimAdminRightsTest.kt
@@ -25,122 +25,124 @@ import org.junit.Test
 class ClaimAdminRightsTest : EndToEndTest() {
   @get:Rule val composeTestRule = createAndroidComposeRule<MainActivity>()
 
-    @Test
-    fun testUserClaimRightsAccess() {
-      /** Create an account on the welcome screen */
-      signInWithUser(composeTestRule, Admin.EMAIL, Admin.PASSWORD)
+  @Test
+  fun testUserClaimRightsAccess() {
+    /** Create an account on the welcome screen */
+    signInWithUser(composeTestRule, Admin.EMAIL, Admin.PASSWORD)
 
-      // Wait until "HomeScreen" is displayed
-      composeTestRule.waitUntil(10000) {
-        composeTestRule.onNodeWithTag(HomeTestTags.SCREEN).isDisplayed()
-      }
+    // Wait until "HomeScreen" is displayed
+    composeTestRule.waitUntil(10000) {
+      composeTestRule.onNodeWithTag(HomeTestTags.SCREEN).isDisplayed()
+    }
 
-      // Wait until the bottom nav bar is displayed
-      composeTestRule.waitUntil(10000) {
-        composeTestRule.onNodeWithTag(BottomNavBarTestTags.MY_PROFILE).isDisplayed()
-      }
+    // Wait until the bottom nav bar is displayed
+    composeTestRule.waitUntil(10000) {
+      composeTestRule.onNodeWithTag(BottomNavBarTestTags.MY_PROFILE).isDisplayed()
+    }
 
-      /** Navigate to the profile screen */
-      composeTestRule.onNodeWithTag(BottomNavBarTestTags.MY_PROFILE).performClick()
+    /** Navigate to the profile screen */
+    composeTestRule.onNodeWithTag(BottomNavBarTestTags.MY_PROFILE).performClick()
 
-      composeTestRule.waitUntil(10000) {
-        composeTestRule.onNodeWithTag(UserProfileTestTags.SCREEN).isDisplayed()
-      }
+    composeTestRule.waitUntil(10000) {
+      composeTestRule.onNodeWithTag(UserProfileTestTags.SCREEN).isDisplayed()
+    }
 
-      /** Navigate to the claiming button screen */
-      composeTestRule.onNodeWithTag(UserProfileTestTags.CLAIMING_BUTTON).performClick()
+    /** Navigate to the claiming button screen */
+    composeTestRule.onNodeWithTag(UserProfileTestTags.CLAIMING_BUTTON).performClick()
 
-      composeTestRule.onNodeWithTag(ExploreContentTestTags.SEARCH_BAR).assertIsDisplayed()
-      composeTestRule
-          .onNodeWithTag(ExploreContentTestTags.SEARCH_BAR_INPUT)
-          .performTextInput(ASSOCIATION_SEARCH_INPUT)
+    composeTestRule.onNodeWithTag(ExploreContentTestTags.SEARCH_BAR).assertIsDisplayed()
+    composeTestRule
+        .onNodeWithTag(ExploreContentTestTags.SEARCH_BAR_INPUT)
+        .performTextInput(ASSOCIATION_SEARCH_INPUT)
 
-      // Wait for the server's response to get the association
-      composeTestRule.waitUntil(10000) {
-        composeTestRule
-            .onNodeWithTag(
-                ExploreContentTestTags.ASSOCIATION_EXPLORE_RESULT + EXPECTED_ASSOCIATION_NAME)
-            .isDisplayed()
-      }
-
+    // Wait for the server's response to get the association
+    composeTestRule.waitUntil(10000) {
       composeTestRule
           .onNodeWithTag(
               ExploreContentTestTags.ASSOCIATION_EXPLORE_RESULT + EXPECTED_ASSOCIATION_NAME)
-          .performClick()
+          .isDisplayed()
+    }
 
-      composeTestRule.waitUntil(5000) {
-        composeTestRule
-            .onNodeWithTag(UserClaimAssociationPresidentialRightsTestTags.SCREEN)
-            .isDisplayed()
-      }
+    composeTestRule
+        .onNodeWithTag(
+            ExploreContentTestTags.ASSOCIATION_EXPLORE_RESULT + EXPECTED_ASSOCIATION_NAME)
+        .performClick()
 
+    composeTestRule.waitUntil(5000) {
       composeTestRule
-          .onNodeWithTag(UserClaimAssociationPresidentialRightsTestTags.EMAIL_ADDRESS)
-          .assertIsDisplayed()
-      composeTestRule
-          .onNodeWithTag(UserClaimAssociationPresidentialRightsTestTags.EMAIL_ADDRESS)
-          .performTextInput(PRESIDENTIAL_EMAIL_ADDRESS)
+          .onNodeWithTag(UserClaimAssociationPresidentialRightsTestTags.SCREEN)
+          .isDisplayed()
+    }
 
-      composeTestRule
-          .onNodeWithTag(UserClaimAssociationPresidentialRightsTestTags.VERIFY_EMAIL_BUTTON)
-          .performClick()
+    composeTestRule
+        .onNodeWithTag(UserClaimAssociationPresidentialRightsTestTags.EMAIL_ADDRESS)
+        .assertIsDisplayed()
+    composeTestRule
+        .onNodeWithTag(UserClaimAssociationPresidentialRightsTestTags.EMAIL_ADDRESS)
+        .performTextInput(PRESIDENTIAL_EMAIL_ADDRESS)
 
-      composeTestRule.waitUntil(5000) {
-        composeTestRule
-            .onNodeWithTag(UserClaimAssociationPresidentialRightsTestTags.CODE)
-            .isDisplayed()
-      }
+    composeTestRule
+        .onNodeWithTag(UserClaimAssociationPresidentialRightsTestTags.VERIFY_EMAIL_BUTTON)
+        .performClick()
 
-      var finalCode = ""
-
-      Firebase.firestore
-          .collection("emailVerifications")
-          .document(EXPECTED_ASSOCIATION_UID)
-          .get()
-          .addOnSuccessListener { document ->
-            Log.d("ClaimHEYHEY", "hehe")
-            if (document != null && document.exists()) {
-              val code: String? = document.getString("code")
-              Log.d("ClaimHEYHEY", "houhou $code") // Retrieve the "code" field as a String
-              if (code != null) {
-                finalCode = code
-                Log.d("ClaimHEYHEY", "hehddd")
-              } else {
-                Log.d("ClaimHEYHEY", "error")
-                throw IllegalStateException("Code field is missing in the document")
-              }
-            } else {
-              Log.d("ClaimHEYHEY", "error2")
-              throw IllegalStateException("Document does not exist")
-            }
-          }
-          .addOnFailureListener { exception ->
-            Log.d("ClaimHEYHEY", "error3")
-            throw IllegalStateException("Failed to fetch verification code: ${exception.message}")
-          }
-
-      Thread.sleep(2000)
-
+    composeTestRule.waitUntil(5000) {
       composeTestRule
           .onNodeWithTag(UserClaimAssociationPresidentialRightsTestTags.CODE)
-          .performTextInput(finalCode)
-      Log.d("ClaimHEYHEY", "houhou $finalCode")
-
-      composeTestRule
-          .onNodeWithTag(UserClaimAssociationPresidentialRightsTestTags.SUBMIT_CODE_BUTTON)
-          .performClick()
-
-      composeTestRule.waitUntil(10000) {
-        composeTestRule.onNodeWithTag(UserProfileTestTags.SCREEN).isDisplayed()
-      }
-
-      signOutWithUser(composeTestRule)
+          .isDisplayed()
     }
 
-    private companion object {
-      const val ASSOCIATION_SEARCH_INPUT = "music"
-      const val EXPECTED_ASSOCIATION_NAME = "Musical"
-      const val PRESIDENTIAL_EMAIL_ADDRESS = "aurelien.domenget@icloud.com"
-      const val EXPECTED_ASSOCIATION_UID = "P0eaFO5qG9y9lK46x8nf"
+    var finalCode = ""
+
+    Firebase.firestore
+        .collection("emailVerifications")
+        .document(EXPECTED_ASSOCIATION_UID)
+        .get()
+        .addOnSuccessListener { document ->
+          Log.d("ClaimHEYHEY", "hehe")
+          if (document != null && document.exists()) {
+            val code: String? = document.getString("code")
+            Log.d("ClaimHEYHEY", "houhou $code") // Retrieve the "code" field as a String
+            if (code != null) {
+              finalCode = code
+              Log.d("ClaimHEYHEY", "hehddd")
+            } else {
+              Log.d("ClaimHEYHEY", "error")
+              throw IllegalStateException("Code field is missing in the document")
+            }
+          } else {
+            Log.d("ClaimHEYHEY", "error2")
+            throw IllegalStateException("Document does not exist")
+          }
+        }
+        .addOnFailureListener { exception ->
+          Log.d("ClaimHEYHEY", "error3")
+          throw IllegalStateException("Failed to fetch verification code: ${exception.message}")
+        }
+
+    Thread.sleep(2000)
+
+    composeTestRule
+        .onNodeWithTag(UserClaimAssociationPresidentialRightsTestTags.CODE)
+        .performTextInput(finalCode)
+    Log.d("ClaimHEYHEY", "houhou $finalCode")
+
+    Thread.sleep(4000)
+
+    composeTestRule
+        .onNodeWithTag(UserClaimAssociationPresidentialRightsTestTags.SUBMIT_CODE_BUTTON)
+        .performClick()
+
+    composeTestRule.waitUntil(10000) {
+      composeTestRule.onNodeWithTag(UserProfileTestTags.SCREEN).isDisplayed()
     }
+
+    signOutWithUser(composeTestRule)
+  }
+
+  private companion object {
+    const val ASSOCIATION_SEARCH_INPUT = "music"
+    const val EXPECTED_ASSOCIATION_NAME = "Musical"
+    const val PRESIDENTIAL_EMAIL_ADDRESS = "aurelien.domenget@icloud.com"
+    const val EXPECTED_ASSOCIATION_UID = "P0eaFO5qG9y9lK46x8nf"
+  }
 }
