b8930085d2b66f0bfe8da15a994dc93917789705
Romain Hirschi <romain.hirschi@epfl.ch>
2024-11-20 13:35:20 +0100

    style: format code with ktfmt

--- app/src/androidTest/java/com/android/unio/end2end/EndToEndTest.kt
+++ app/src/androidTest/java/com/android/unio/end2end/EndToEndTest.kt
@@ -9,7 +9,6 @@ import androidx.compose.ui.test.performTextInput
 import com.android.unio.model.strings.test_tags.BottomNavBarTestTags
 import com.android.unio.model.strings.test_tags.UserProfileTestTags
 import com.android.unio.model.strings.test_tags.WelcomeTestTags
-import com.android.unio.ui.assertDisplayComponentInScroll
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 import com.google.firebase.firestore.firestore
