18fd5e4af0c9f44218f8f4a32f827c85b258d6ab
Zafar <isadjanovzafar@gmail.com>
2024-11-25 17:32:02 +0100

    refactor(location): add Place ID and address properties to data class

--- app/src/main/java/com/android/unio/model/map/Location.kt
+++ app/src/main/java/com/android/unio/model/map/Location.kt
@@ -1,3 +1,3 @@
 package com.android.unio.model.map
 
-data class Location(val latitude: Double = 0.0, val longitude: Double = 0.0, val name: String = "")
+data class Location(val placeId: String = "", val name: String = "", val address: String = "", val latitude: Double = 0.0, val longitude: Double = 0.0)
