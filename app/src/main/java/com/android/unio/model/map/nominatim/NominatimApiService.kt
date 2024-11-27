013040e0304693dede32765009d6e694dbcf0879
Zafar <isadjanovzafar@gmail.com>
2024-11-27 21:19:47 +0100

    chore(map-nominatim): add NominatimApiService defining url search for Retrofit
    This interface is required by Retrofit to build the URL to make the query.

---
+++ app/src/main/java/com/android/unio/model/map/nominatim/NominatimApiService.kt
@@ -0,0 +1,14 @@
+package com.android.unio.model.map.nominatim
+
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface NominatimApiService {
+  @Headers("User-Agent: Unio/0.3 (zafarderie@gmail.com)")
+  @GET("search")
+  suspend fun search(
+      @Query("q") query: String,
+      @Query("format") format: String = "json"
+  ): List<NominatimLocationResponse>
+}
