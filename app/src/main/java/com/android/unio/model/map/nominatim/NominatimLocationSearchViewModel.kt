1d189eb15bebdfb1f490e9aef059ebec89875e4d
Zafar <isadjanovzafar@gmail.com>
2024-11-28 22:59:25 +0100

    chore(nominatim): add documentation to NominatimLocationSearchViewModel

--- app/src/main/java/com/android/unio/model/map/nominatim/NominatimLocationSearchViewModel.kt
+++ app/src/main/java/com/android/unio/model/map/nominatim/NominatimLocationSearchViewModel.kt
@@ -11,25 +11,35 @@ import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 
+/**
+ * ViewModel for searching locations using Nominatim API.
+ *
+ * @param repository LocationRepository : Repository for searching locations.
+ */
 @HiltViewModel
 class NominatimLocationSearchViewModel
 @Inject
 constructor(private val repository: LocationRepository) : ViewModel() {
 
+  /** Query for searching locations. */
   private val _query = MutableStateFlow("")
   val query: StateFlow<String> = _query.asStateFlow()
 
+  /** List of location suggestions. */
   private val _locationSuggestions = MutableStateFlow<List<Location>>(emptyList())
   val locationSuggestions: StateFlow<List<Location>> = _locationSuggestions.asStateFlow()
 
-  fun setQuery(query: String) {
-    _query.value = query
-  }
-
+  /** Launches the ViewModel scope. */
   init {
     collectQuery()
   }
 
+  /**
+   * Collects query and fetches location suggestions, using flows.
+   *
+   * Uses debounce to wait for user to stop typing. Uses flatMapLatest to only show the latest
+   * search. Uses catch to handle exceptions and return an empty list for suggestions.
+   */
   @OptIn(FlowPreview::class, ExperimentalCoroutinesApi::class)
   private fun collectQuery() {
     viewModelScope.launch {
@@ -39,5 +49,14 @@ constructor(private val repository: LocationRepository) : ViewModel() {
           .flatMapLatest { query -> repository.search(query).catch { emit(emptyList()) } }
           .collect { locations -> _locationSuggestions.value = locations }
     }
+  }
+
+  /**
+   * Sets the query for searching locations.
+   *
+   * @param query String : Query for searching locations.
+   */
+  fun setQuery(query: String) {
+    _query.value = query
   }
 }
