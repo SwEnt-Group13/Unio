287e34ecd1442a2b84e4be6b7ad06cfcff547680
Alexei Thornber <alexeithornber@gmail.com>
2024-12-05 11:11:33 +0100

    feat(Utils): Add `checkInputLength` methods

--- app/src/main/java/com/android/unio/model/utils/Utils.kt
+++ app/src/main/java/com/android/unio/model/utils/Utils.kt
@@ -11,3 +11,11 @@ object Utils {
     return connectivityManager?.activeNetwork != null
   }
 }
+
+fun checkInputLengthShort(input: String) : Boolean {
+  return input.length in 1..29
+}
+
+fun checkInputLengthLong(input: String) : Boolean {
+  return input.length in 1..199
+}
