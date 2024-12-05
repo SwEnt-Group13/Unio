07af0b8aa51c6ea74e88cd7dfc25013b631b90e7
Aurelien9Code <152640312+Aurelien9Code@users.noreply.github.com>
2024-12-05 14:12:28 +0100

    style(manage-pictures): format using ktfmt formating

--- app/src/main/java/com/android/unio/ui/components/UserEditionComponents.kt
+++ app/src/main/java/com/android/unio/ui/components/UserEditionComponents.kt
@@ -1,11 +1,6 @@
 package com.android.unio.ui.components
 
-import android.content.ContentValues
 import android.net.Uri
-import android.provider.MediaStore
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.PickVisualMediaRequest
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -13,20 +8,21 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.ModalBottomSheetLayout
-
-import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.rounded.AccountCircle
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.InputChip
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -42,11 +38,6 @@ import com.android.unio.model.user.Interest
 import com.android.unio.model.user.UserSocial
 import com.android.unio.ui.image.AsyncImageWrapper
 import com.android.unio.ui.theme.primaryLight
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.SheetState
-import androidx.compose.material3.rememberModalBottomSheetState
-import androidx.compose.runtime.rememberCoroutineScope
 import kotlinx.coroutines.launch
 
 @Composable
@@ -73,8 +64,6 @@ private fun ProfilePictureWithRemoveIcon(
   }
 }
 
-
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfilePicturePicker(
@@ -82,57 +71,51 @@ fun ProfilePicturePicker(
     onProfilePictureUriChange: () -> Unit,
     testTag: String
 ) {
-    val context = LocalContext.current
-    val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded  = true)
-    val scope = rememberCoroutineScope()
-    var showSheet by remember { mutableStateOf(false) }
+  val context = LocalContext.current
+  val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+  val scope = rememberCoroutineScope()
+  var showSheet by remember { mutableStateOf(false) }
 
-    if (profilePictureUri.value == Uri.EMPTY) {
-        Icon(
-            imageVector = Icons.Rounded.AccountCircle,
-            contentDescription = context.getString(R.string.account_details_content_description_add),
-            tint = primaryLight,
-            modifier = Modifier
-                .clickable {
-                    showSheet = true
+  if (profilePictureUri.value == Uri.EMPTY) {
+    Icon(
+        imageVector = Icons.Rounded.AccountCircle,
+        contentDescription = context.getString(R.string.account_details_content_description_add),
+        tint = primaryLight,
+        modifier = Modifier.clickable { showSheet = true }.size(100.dp).testTag(testTag))
+  } else {
+    ProfilePictureWithRemoveIcon(
+        profilePictureUri = profilePictureUri.value, onRemove = onProfilePictureUriChange)
+  }
+
+  if (showSheet) {
+    ModalBottomSheet(
+        sheetState = sheetState,
+        onDismissRequest = {
+          scope.launch {
+            sheetState.hide()
+            showSheet = false
+          }
+        },
+        content = {
+          PictureSelectionTool(
+              maxPictures = 1,
+              allowGallery = true,
+              allowCamera = true,
+              onValidate = { uris ->
+                if (uris.isNotEmpty()) {
+                  profilePictureUri.value =
+                      uris.first() // Use the first selected as the profile picture
                 }
-                .size(100.dp)
-                .testTag(testTag)
-        )
-    } else {
-        ProfilePictureWithRemoveIcon(
-            profilePictureUri = profilePictureUri.value,
-            onRemove = onProfilePictureUriChange
-        )
-    }
-
-    if (showSheet) {
-        ModalBottomSheet(
-            sheetState = sheetState,
-            onDismissRequest = { scope.launch { sheetState.hide(); showSheet=false } },
-            content = {
-                PictureSelectionTool(
-                    maxPictures = 1,
-                    allowGallery = true,
-                    allowCamera = true,
-                    onValidate = { uris ->
-                        if (uris.isNotEmpty()) {
-                            profilePictureUri.value = uris.first() // Use the first selected as the profile picture
-                        }
-                        scope.launch { sheetState.hide() }
-                        showSheet = false
-                    },
-                    onCancel = {
-                        scope.launch { sheetState.hide() }
-                        showSheet = false
-                    }
-                )
-            }
-        )
-    }
+                scope.launch { sheetState.hide() }
+                showSheet = false
+              },
+              onCancel = {
+                scope.launch { sheetState.hide() }
+                showSheet = false
+              })
+        })
+  }
 }
-
-
 
 @Composable
 fun InterestInputChip(pair: Pair<Interest, MutableState<Boolean>>, testTag: String) {
