24f0d97f3689590b35fb37702356ef896bb6df7e
Oskar Codes <oskarbertrand@icloud.com>
2024-12-05 15:19:55 +0100

    chore(ImageViewModel): Create an ImageViewModel acting as an interface between the view and the repository

---
+++ app/src/main/java/com/android/unio/model/image/ImageViewModel.kt
@@ -0,0 +1,21 @@
+package com.android.unio.model.image
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import java.io.InputStream
+import javax.inject.Inject
+
+@HiltViewModel
+class ImageViewModel @Inject constructor(private val repository: ImageRepository) : ViewModel() {
+  fun uploadImage(
+      inputStream: InputStream,
+      path: String,
+      onSuccess: (String) -> Unit,
+      onFailure: () -> Unit
+  ) {
+    repository.uploadImage(inputStream, path, onSuccess) { exception ->
+      Log.e("ImageViewModel", "Error uploading image: $exception")
+    }
+  }
+}
