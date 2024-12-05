0e9958f505482aa259f025317a11ff091d916032
Arnaud Rajon <arnaud.rajon@epfl.ch>
2024-12-05 21:04:53 +0100

    Create EventUserPicture.kt

---
+++ app/src/main/java/com/android/unio/model/event/EventUserPicture.kt
@@ -0,0 +1,11 @@
+package com.android.unio.model.event
+
+import com.android.unio.model.firestore.UniquelyIdentifiable
+import com.android.unio.model.user.User
+
+data class EventUserPicture(
+    override val uid: String,
+    val image: String,
+    val author: User,
+    val likes: Int
+) : UniquelyIdentifiable
