9771bc97efb1e39b18940eef7b39815b40c5f897
Redd87 <eddy.p.rashed@gmail.com>
2024-12-08 19:28:45 +0100

    style: ktfmt

--- app/src/main/java/com/android/unio/ui/association/AssociationSmall.kt
+++ app/src/main/java/com/android/unio/ui/association/AssociationSmall.kt
@@ -4,11 +4,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.TextButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForwardIos
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
