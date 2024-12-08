a43971cfa5b6c20f0339567900423dec99c7c229
Oskar Codes <oskarbertrand@icloud.com>
2024-12-08 15:28:28 +0100

    feat(CloudMessaging): Add association followers notification target and better error handling

--- app/src/main/java/com/android/unio/model/notification/CloudMessaging.kt
+++ app/src/main/java/com/android/unio/model/notification/CloudMessaging.kt
@@ -6,25 +6,36 @@ import android.app.NotificationManager
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.android.unio.R
+import com.android.unio.model.strings.NotificationStrings.EVENT_BROADCAST_CHANNEL_DESCRIPTION
 import com.android.unio.model.strings.NotificationStrings.EVENT_BROADCAST_CHANNEL_ID
-import com.android.unio.model.strings.NotificationStrings.EVENT_BROADCAST_NOTIFICATION_ID
+import com.android.unio.model.strings.NotificationStrings.EVENT_BROADCAST_CHANNEL_NAME
 import com.google.firebase.Firebase
 import com.google.firebase.functions.functions
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
-enum class NotificationType(val requiredFields: List<String>) {
-  EVENT_MESSAGE(listOf("title", "body")),
+enum class NotificationTarget(val requiredFields: List<String>) {
+  EVENT_SAVERS(listOf("title", "body")),
+  ASSOCIATION_FOLLOWERS(listOf("title", "body")),
 }
 
+/**
+ * Broadcasts a message to all users subscribed to a topic. This is a client-side function that
+ * calls a Firebase Cloud Function, as message broadcasting is a server-side operation.
+ *
+ * @param type The type of notification to broadcast.
+ * @param topic The topic to broadcast the message to.
+ * @param payload The data to send with the message.
+ * @param onSuccess The function to call if the message is successfully sent.
+ */
 fun broadcastMessage(
-    type: NotificationType,
+    type: NotificationTarget,
     topic: String,
-    payload: Map<String, Any>,
+    payload: Map<String, String>,
     onSuccess: () -> Unit,
     onFailure: () -> Unit
 ) {
-  // check for required fields in data according to type
+  // Check if the payload contains all required fields
   type.requiredFields.forEach {
     if (!payload.containsKey(it)) {
       Log.e("CloudMessaging", "broadcastMessage: missing required field $it")
@@ -44,33 +55,47 @@ fun broadcastMessage(
 }
 
 class UnioMessagingService : FirebaseMessagingService() {
-  override fun onMessageReceived(message: RemoteMessage) {
-    println("Message received: ${message.data}")
+  override fun onNewToken(token: String) {
+    // Method is left empty because we do not target single devices
+  }
 
-    if (!message.data.containsKey("type")) {
+  override fun onMessageReceived(message: RemoteMessage) {
+    Log.i("CloudMessaging", "onMessageReceived: message ${message.data}")
+
+    // Get the type and topic of the notification
+    val typeString = message.data["type"]
+    if (typeString == null) {
       Log.e("CloudMessaging", "onMessageReceived: missing type field")
       return
     }
+    val type = NotificationTarget.valueOf(typeString)
 
-    val type = NotificationType.valueOf(message.data["type"]!!)
+    // Check if the notification has the required fields
+    type.requiredFields.forEach {
+      if (!message.data.containsKey(it)) {
+        Log.e(
+            "CloudMessaging",
+            "onMessageReceived: message ${message.data} missing required field $it")
+        return
+      }
+    }
 
+    // Build notification
     var builder =
         NotificationCompat.Builder(this, EVENT_BROADCAST_CHANNEL_ID)
             .setSmallIcon(R.drawable.other_icon)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
 
+    // Set notification content based on type
     when (type) {
-      NotificationType.EVENT_MESSAGE -> {
-        if (!message.data.containsKey("title") || !message.data.containsKey("body")) {
-          Log.e("CloudMessaging", "onMessageReceived: missing title or body field")
-          return
-        }
-
+      NotificationTarget.EVENT_SAVERS,
+      NotificationTarget.ASSOCIATION_FOLLOWERS -> {
         builder =
             builder.setContentTitle(message.data["title"]).setContentText(message.data["body"])
       }
     }
 
+    // Create notification channel if it does not exist
     val notificationManager: NotificationManager =
         getSystemService(NOTIFICATION_SERVICE) as NotificationManager
 
@@ -79,15 +104,17 @@ class UnioMessagingService : FirebaseMessagingService() {
       notificationChannel =
           NotificationChannel(
               EVENT_BROADCAST_CHANNEL_ID,
-              EVENT_BROADCAST_CHANNEL_ID,
+              EVENT_BROADCAST_CHANNEL_NAME,
               NotificationManager.IMPORTANCE_HIGH)
       notificationChannel.enableLights(true)
       notificationChannel.setShowBadge(true)
       notificationChannel.lockscreenVisibility = Notification.VISIBILITY_PUBLIC
+      notificationChannel.description = EVENT_BROADCAST_CHANNEL_DESCRIPTION
 
       notificationManager.createNotificationChannel(notificationChannel)
     }
 
-    notificationManager.notify(EVENT_BROADCAST_NOTIFICATION_ID, builder.build())
+    // Send notification
+    notificationManager.notify(System.currentTimeMillis().toInt(), builder.build())
   }
 }
