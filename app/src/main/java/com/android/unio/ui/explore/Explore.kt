af2d1ae688147d851c544e3975a4ea5d5a6899a2
Zafar <isadjanovzafar@gmail.com>
2024-12-09 13:48:28 +0100

    chore: optimize imports

--- app/src/main/java/com/android/unio/ui/explore/Explore.kt
+++ app/src/main/java/com/android/unio/ui/explore/Explore.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
