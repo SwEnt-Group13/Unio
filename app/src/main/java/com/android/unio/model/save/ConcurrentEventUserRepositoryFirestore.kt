52602350af635608d12597ba59160d071e08c250
Arnaud Rajon <arnaud.rajon@epfl.ch>
2024-12-12 16:25:46 +0100

    feat: implement updateSave method

--- app/src/main/java/com/android/unio/model/save/ConcurrentEventUserRepositoryFirestore.kt
+++ app/src/main/java/com/android/unio/model/save/ConcurrentEventUserRepositoryFirestore.kt
@@ -1,9 +1,9 @@
 package com.android.unio.model.save
 
-import com.android.unio.model.association.AssociationRepositoryFirestore
 import com.android.unio.model.authentication.registerAuthStateListener
 import com.android.unio.model.event.Event
 import com.android.unio.model.event.EventRepository
+import com.android.unio.model.event.EventRepositoryFirestore
 import com.android.unio.model.firestore.transform.serialize
 import com.android.unio.model.user.User
 import com.android.unio.model.user.UserRepository
@@ -28,6 +28,7 @@ constructor(
     private val userRepository: UserRepository,
     private val eventRepository: EventRepository
 ) : ConcurrentEventUserRepository {
+
   override fun init(onSuccess: () -> Unit) {
     Firebase.auth.registerAuthStateListener {
       if (it.currentUser != null) {
@@ -36,6 +37,15 @@ constructor(
     }
   }
 
+  /**
+   * Updates the Firestore database with the user's save status for an event. This operation is
+   * performed atomically. If the operation fails, the database is not updated.
+   *
+   * @param user The user.
+   * @param event The event.
+   * @param onSuccess The callback that is called when the operation is successful.
+   * @param onFailure The callback that is called when the operation fails.
+   */
   override fun updateSave(
       user: User,
       event: Event,
@@ -44,9 +54,9 @@ constructor(
   ) {
     db.runBatch { batch ->
           val userRef = userRepository.getUserRef(user.uid)
-          val associationRef = eventRepository.getEventRef(association.uid)
+          val eventRef = eventRepository.getEventRef(event.uid)
 
-          batch.set(associationRef, AssociationRepositoryFirestore.serialize(association))
+          batch.set(eventRef, EventRepositoryFirestore.serialize(event))
           batch.set(userRef, UserRepositoryFirestore.serialize(user))
         }
         .addOnSuccessListener { onSuccess() }
