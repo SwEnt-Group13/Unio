3385c3dc31a3576c54c0bf293a9011c97382ed5e
Arnaud Rajon <arnaud.rajon@epfl.ch>
2024-12-15 14:53:37 +0100

    feat: create PictureOverlay

---
+++ app/src/main/java/com/android/unio/ui/event/overlay/PictureOverlay.kt
@@ -0,0 +1,9 @@
+package com.android.unio.ui.event.overlay
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.window.Dialog
+
+@Composable
+fun PictureOverlay() {
+    Dialog(onDismissRequest = {}) { }
+}
