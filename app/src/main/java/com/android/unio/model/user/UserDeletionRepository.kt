36caaeca579b7adf80f20fc774f069ace233e073
Romain Hirschi <romain.hirschi@epfl.ch>
2024-12-16 11:12:39 +0100

    refactor(UserDeletion): add repository file to delete users

---
+++ app/src/main/java/com/android/unio/model/user/UserDeletionRepository.kt
@@ -0,0 +1,4 @@
+package com.android.unio.model.user
+
+interface UserDeletionRepository {
+}
---
+++ app/src/main/java/com/android/unio/model/user/UserDeletionRepositoryFirestore.kt
@@ -0,0 +1,4 @@
+package com.android.unio.model.user
+
+class UserDeletionRepositoryFirestore : UserDeletionRepository {
+}
