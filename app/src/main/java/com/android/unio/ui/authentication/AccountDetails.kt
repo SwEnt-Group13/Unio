537cc9f77846f83dc8002b93e266376a133c9974
Zafar <isadjanovzafar@gmail.com>
2024-12-16 19:00:27 +0100

    feat(account-details): add clear button to first and last name input fields

--- app/src/main/java/com/android/unio/ui/authentication/AccountDetails.kt
+++ app/src/main/java/com/android/unio/ui/authentication/AccountDetails.kt
@@ -15,8 +15,10 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.outlined.Clear
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -306,7 +308,20 @@ private fun UserTextFields(
           onFirstNameChange(it)
         }
       },
-      value = firstName)
+      value = firstName,
+      trailingIcon = {
+        if (firstName.isNotEmpty()) {
+          IconButton(
+              onClick = { onFirstNameChange("") },
+              modifier = Modifier.testTag(AccountDetailsTestTags.FIRST_NAME_CLEAR_BUTTON)) {
+                Icon(
+                    imageVector = Icons.Outlined.Clear,
+                    contentDescription =
+                        context.getString(
+                            R.string.account_details_content_description_clear_first_name))
+              }
+        }
+      })
 
   OutlinedTextField(
       modifier =
@@ -340,7 +355,20 @@ private fun UserTextFields(
           onLastNameChange(it)
         }
       },
-      value = lastName)
+      value = lastName,
+      trailingIcon = {
+        if (lastName.isNotEmpty()) {
+          IconButton(
+              onClick = { onLastNameChange("") },
+              modifier = Modifier.testTag(AccountDetailsTestTags.LAST_NAME_CLEAR_BUTTON)) {
+                Icon(
+                    imageVector = Icons.Outlined.Clear,
+                    contentDescription =
+                        context.getString(
+                            R.string.account_details_content_description_clear_last_name))
+              }
+        }
+      })
 
   OutlinedTextField(
       modifier =
