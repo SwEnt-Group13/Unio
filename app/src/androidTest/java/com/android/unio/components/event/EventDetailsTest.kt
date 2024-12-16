8acf638c75004da814e44e19a1a91b3c590c3b7c
Romain Hirschi <romain.hirschi@epfl.ch>
2024-12-16 15:28:46 +0100

    refactor: update code according to user deletion change

--- app/src/androidTest/java/com/android/unio/components/event/EventDetailsTest.kt
+++ app/src/androidTest/java/com/android/unio/components/event/EventDetailsTest.kt
@@ -29,6 +29,7 @@ import com.android.unio.model.save.ConcurrentEventUserRepositoryFirestore
 import com.android.unio.model.strings.FormatStrings.DAY_MONTH_FORMAT
 import com.android.unio.model.strings.test_tags.event.EventDetailsTestTags
 import com.android.unio.model.user.User
+import com.android.unio.model.user.UserDeletionRepositoryFirestore
 import com.android.unio.model.user.UserRepositoryFirestore
 import com.android.unio.model.user.UserViewModel
 import com.android.unio.ui.event.EventScreenScaffold
@@ -64,6 +65,7 @@ class EventDetailsTest : TearDown() {
   @MockK private lateinit var eventRepository: EventRepositoryFirestore
   @MockK private lateinit var associationRepository: AssociationRepositoryFirestore
   @MockK private lateinit var userRepository: UserRepositoryFirestore
+  @MockK private lateinit var userDeletionRepository: UserDeletionRepositoryFirestore
   @MockK private lateinit var imageRepository: ImageRepositoryFirebaseStorage
   @MockK
   private lateinit var eventUserPictureRepositoryFirestore: EventUserPictureRepositoryFirestore
@@ -128,7 +130,7 @@ class EventDetailsTest : TearDown() {
           (it.invocation.args[1] as (User) -> Unit)((MockUser.createMockUser()))
         }
 
-    userViewModel = UserViewModel(userRepository, imageRepository)
+    userViewModel = UserViewModel(userRepository, imageRepository, userDeletionRepository)
     userViewModel.getUserByUid("uid")
   }
 
